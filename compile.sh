59436c0de5b54cb0309f00626b48c32a1b835f83
Klaykap <43180618+Klaykap@users.noreply.github.com>
2020-10-10 20:51:24 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-echo "Compiling BleskOS..."
-nasm -f bin bleskos.asm -o bleskos.bin
-
-echo "Compiling BleskOS bootloader..."
+nasm -f bin bleskos.asm -o bleskos_images/bleskos.bin
 nasm -f bin bootloader/bootloader.asm -o bootloader/bootloader.bin
 
-echo "Erasing image..."
-dd if=/dev/zero of=bleskos.img bs=1024 count=1440
+dd if=/dev/zero of=bleskos_images/bleskos.img bs=1024 count=1440
+dd if=bootloader/bootloader.bin of=bleskos_images/bleskos.img conv=notrunc seek=0
+dd if=bleskos_images/bleskos.bin of=bleskos_images/bleskos.img conv=notrunc seek=1
 
-echo "Copying bootloader..."
-dd if=bootloader/bootloader.bin of=bleskos.img conv=notrunc seek=0
-
-echo "Copying BleskOS..."
-dd if=bleskos.bin of=bleskos.img conv=notrunc seek=1
-
-echo "Starting Qemu..."
-qemu-system-i386 -soundhw pcspk -soundhw hda -net nic,model=rtl8139 -net user -hda bleskos.img
+qemu-system-i386 -soundhw pcspk -hda bleskos_images/bleskos.img
 
 sleep 50
