84a52e505d924d338c5e7fda60df04332084093c
Klaykap <43180618+Klaykap@users.noreply.github.com>
2020-10-12 15:18:53 +0200

    Update core.asm

--- core/core.asm
+++ core/core.asm
@@ -1,40 +1,20 @@
-;BleskOS real mode edition
+;BleskOS real mode
 
 init_core:
+  mov byte [boot_device_number], dl
   mov bl, 0x20
   call draw_background
-
-  mov ah, 0x02
-  mov bh, 0
-  mov dx, 0x0101
-  int 10h
-  mov si, str1
-  call print
-
-  mov ah, 0x02
-  mov bh, 0
-  mov dx, 0x0201
-  int 10h
-  mov si, str2
-  call print
   call stop_pc_speaker
-
-  mov ah, 0x02
-  mov bh, 0
-  mov dx, 0x0301
-  int 10h
-  mov si, str3
-  call print
   mov byte [keyboard_langunge], 2
+  call read_time
 
   ret
-
-  str1 db '> initalizing core', 0
-  str2 db '> stopping pc speaker', 0
-  str3 db '> setting keyboard', 0
 
 %include "core/vga.asm"
 %include "core/pc_speaker.asm"
 %include "core/keyboard.asm"
 %include "core/memory_devices.asm"
 %include "core/jus.asm"
+%include "core/time.asm"
+%include "core/shutdown.asm"
+%include "core/wait.asm"
