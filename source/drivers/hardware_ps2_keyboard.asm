b20062d9ceefb4d9b9fba1fd008b468005e6ec5a
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-01-19 20:10:39 +0100

    Update hardware_ps2_keyboard.asm

--- source/drivers/hardware_ps2_keyboard.asm
+++ source/drivers/hardware_ps2_keyboard.asm
@@ -21,9 +21,21 @@ keyboard_wait db 0
 selected_keyboard_set dd 0
 
 english_keyboard_layout:
- times 256 db ' '
+ db ' ', ' ' ;zero, escape
+ db '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', ' ', ' ' ;first line, backspace, tab
+ db 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', ' ', ' ' ;second line, enter, left ctrl
+ db 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', ' ', '\' ;third line, left shift
+ db 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', ' ' ;fourth line, right shift
+ times 15 db ' ' ;other control keys
+ db '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.' ;numpad
 english_shift_keyboard_layout:
- times 256 db ' '
+ db ' ', ' ' ;zero, escape
+ db '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', ' ', ' ' ;first line, backspace, tab
+ db 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', ' ', ' ' ;second line, enter, left ctrl
+ db 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '¨', '~', ' ', '|' ;third line, left shift
+ db 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', ' ' ;fourth line, right shift
+ times 15 db ' ' ;other control keys
+ db '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.' ;numpad
 
 init_keyboard:
  mov dword [keyboard_shift], 0
