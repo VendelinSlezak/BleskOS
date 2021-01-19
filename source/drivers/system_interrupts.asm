b7fc02e8f4f6bc820f76f7448a1e808a02b4ab94
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-01-19 18:37:55 +0100

    Create system_interrupts.asm

---
+++ source/drivers/system_interrupts.asm
@@ -0,0 +1,52 @@
+;BleskOS
+
+%define EMPTY_IDT_ENTRY dq 0
+
+%macro IDT_ENTRY 1
+ dw %1
+ dw 0x0008
+ db 0, 0x8E
+ dw 0x0001
+%endmacro
+
+%macro EOI_MASTER_PIC 0
+ OUTB 0x20, 0x20
+%endmacro
+
+%macro EOI_SLAVE_PIC 0
+ OUTB 0xA0, 0x20
+ OUTB 0x20, 0x20
+%endmacro
+
+idt:
+ times 32 EMPTY_IDT_ENTRY
+ IDT_ENTRY timer_irq
+ IDT_ENTRY keyboard_irq
+ times 256-34 EMPTY_IDT_ENTRY
+
+idt_wrap:
+ dw 2047 ;lenght
+ dd idt ;address
+
+init_idt:
+ cli
+
+ ;remapping PIC
+ OUTB 0x20, 0x11
+ OUTB 0xA0, 0x11
+ OUTB 0x21, 0x20
+ OUTB 0xA1, 40
+ OUTB 0x21, 0x04
+ OUTB 0xA1, 0x02
+ OUTB 0x21, 0x01
+ OUTB 0xA1, 0x01
+ OUTB 0x21, 0xFC ;irq 0, 1
+ OUTB 0xA1, 0xFF ;no irq
+
+ ;load idt
+ mov edx, idt_wrap
+ lidt [edx]
+
+ sti
+
+ ret
