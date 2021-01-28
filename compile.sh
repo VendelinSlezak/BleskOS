594d49bf3e355ce41539900e5947124fc0b84227
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-01-28 20:23:56 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -9,6 +9,6 @@ dd if=/dev/zero of=images/bleskos.hdd bs=1024 count=1000
 dd if=bootloader/bootloader.bin of=images/bleskos.hdd conv=notrunc seek=0
 dd if=images/bleskos.bin of=images/bleskos.hdd conv=notrunc seek=1
 
-qemu-system-i386 -m 64 -usbdevice mouse -soundhw hda -hda images/bleskos.hdd
+qemu-system-i386 -m 64 -soundhw ac97 -hda images/bleskos.hdd
 
 sleep 60
