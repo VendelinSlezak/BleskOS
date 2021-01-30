f4939c842adb3546b1eedfa8f3b08d49338db163
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-01-30 15:40:10 +0100

    Update hardware_ps2_keyboard.asm

--- source/drivers/hardware_ps2_keyboard.asm
+++ source/drivers/hardware_ps2_keyboard.asm
@@ -1,5 +1,4 @@
 ;BleskOS
-
 %define KEY_ESC 0x01
 %define KEY_ENTER 0x1C
 %define KEY_TAB 0x0F
@@ -9,10 +8,18 @@
 %define KEY_DELETE 0x53
 %define KEY_LEFT_SHIFT 0x2A
 %define KEY_RIGHT_SHIFT 0x36
+
 %define KEY_UP 0x48 
 %define KEY_DOWN 0x50
 %define KEY_LEFT 0x4B
 %define KEY_RIGHT 0x4D
+
+%define KEY_F1 0x3B
+%define KEY_F2 0x3C
+%define KEY_F3 0x3D
+%define KEY_F4 0x3E
+
+%define KEY_A 0x1E
 
 key_code dd 0
 keyboard_shift dd 0
