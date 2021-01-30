2c8427b56d7c80add7ca00585e45e50a8b368e96
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-01-30 16:17:02 +0100

    Update sound_center.asm

--- source/drivers/sound_center.asm
+++ source/drivers/sound_center.asm
@@ -1,8 +1,13 @@
 ;BleskOS
 
+%define SOUND_CARD_AC97 1
+%define SOUND_CARD_HDA 2
+
 sound_memory dd 0
 sound_volume dw 0
 sound_sample_rate dw 0
+
+sound_card_type dd 0
 
 %macro SET_SOUND_MEMORY 1
  mov dword [sound_memory], %1
@@ -19,8 +24,11 @@ sound_sample_rate dw 0
 %endmacro
 
 init_sound_card:
+ mov dword [sound_card_type], 0
+
  IF_NE word [ac97_nam_base], 0, if_ac97
   call init_ac97
+  mov dword [sound_card_type], SOUND_CARD_AC97
  ENDIF if_ac97
 
  ret
