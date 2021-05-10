dbefb3edb3344772eb72fc7faabcc6de5dc63785
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-05-10 13:01:34 +0200

    Update hardware_ps2_keyboard.asm

--- source/drivers/hardware_ps2_keyboard.asm
+++ source/drivers/hardware_ps2_keyboard.asm
@@ -1,4 +1,5 @@
 ;BleskOS
+
 %define KEY_ESC 0x01
 %define KEY_ENTER 0x1C
 %define KEY_TAB 0x0F
@@ -20,6 +21,42 @@
 %define KEY_F4 0x3E
 
 %define KEY_A 0x1E
+%define KEY_B 0x30
+%define KEY_C 0x2E
+%define KEY_D 0x20
+%define KEY_E 0x12
+%define KEY_F 0x21
+%define KEY_G 0x22
+%define KEY_H 0x23
+%define KEY_I 0x17
+%define KEY_J 0x24
+%define KEY_K 0x25
+%define KEY_L 0x26
+%define KEY_M 0x32
+%define KEY_N 0x31
+%define KEY_O 0x18
+%define KEY_P 0x19
+%define KEY_Q 0x10
+%define KEY_R 0x13
+%define KEY_S 0x1F
+%define KEY_T 0x14
+%define KEY_U 0x16
+%define KEY_V 0x2F
+%define KEY_W 0x11
+%define KEY_X 0x2D
+%define KEY_Y 0x15
+%define KEY_Z 0x2C
+
+%define KEY_1 0x02
+%define KEY_2 0x03
+%define KEY_3 0x04
+%define KEY_4 0x05
+%define KEY_5 0x06
+%define KEY_6 0x07
+%define KEY_7 0x08
+%define KEY_8 0x09
+%define KEY_9 0x0A
+%define KEY_0 0x0B
 
 key_code dd 0
 keyboard_shift dd 0
@@ -50,6 +87,7 @@ init_keyboard:
 
 keyboard_irq:
  pusha
+
  mov eax, 0
  INB 0x60
  mov dword [key_code], eax
