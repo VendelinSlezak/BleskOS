3a15a0d4d0fc052431f15b9aed5953efd0cb0945
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-06-21 19:24:30 +0200

    Update hardware_storage_center.asm

--- source/drivers/hardware_storage_center.asm
+++ source/drivers/hardware_storage_center.asm
@@ -20,28 +20,6 @@ ata_status dd 0
 %endmacro
 
 init_ide_devices:
- cmp word [sata_base], 0
- je .init_pata
-
- PSTR 'Serial ATA', serial_ata
-
- ;SATA_OUT 0x28, 0x2 ;disable BIOS ownership
- ;WAIT 2
-
- ;SATA_OUT 0x04, 0x80000000 ;enable AHCI mode
-
- ;SATA_OUT 0x118, 0 ;port 1 command register
-
- SATA_IN 0x10
- PHEX eax
-
- SATA_IN 0x124
- PHEX eax
-
- ret
-
- .init_pata:
- PSTR 'Paralel ATA', paralel_ata
  mov word [pata_base], 0x1F0
  OUTB 0x1F6, 0x40 ;master drive
  WAIT 2 ;wait for enable drive
@@ -50,21 +28,9 @@ init_ide_devices:
  ret
 
 read_hdd:
- cmp dword [sata_base], 0
- je .pata_read
- call sata_read
- ret
-
- .pata_read:
  call pata_read
  ret
 
 write_hdd:
- cmp dword [sata_base], 0
- je .pata_write
- call sata_write
- ret
-
- .pata_write:
  call pata_write
  ret
