9fbffce02ab025bab85ee9b4b8f24c130d4c857d
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-06-24 21:05:01 +0200

    Update macros.asm

--- source/macros.asm
+++ source/macros.asm
@@ -6,7 +6,10 @@
 %define MEMORY_JUS_BNF 0x00200000
 %define MEMORY_FILE_BLOCK_NUMBERS 0x00300000
 %define MEMORY_ROOT_DIR 0x00400000
-%define MEMORY_HDA_BUFFER 0x00510000
+%define MEMORY_HDA_BUFFER 0x00500000
+%define MEMORY_HDA_CORB 0x00510000
+%define MEMORY_HDA_RIRB 0x00520000
+%define MEMORY_HDA_DMAPOS 0x00530000
 %define MEMORY_UHCI 0x00600000
 %define MEMORY_EHCI 0x00700000
 
@@ -17,6 +20,18 @@
  in al, dx
 %endmacro
 
+%macro BASE_INB 2
+ mov dx, word [%1]
+ add dx, %2
+ in al, dx
+%endmacro
+
+%macro MMIO_INB 2
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov al, byte [ebp]
+%endmacro
+
 %macro INW 1
  %if %1!=dx
  mov dx, %1
@@ -24,11 +39,35 @@
  in ax, dx
 %endmacro
 
+%macro BASE_INW 2
+ mov dx, word [%1]
+ add dx, %2
+ in ax, dx
+%endmacro
+
+%macro MMIO_INW 2
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov ax, word [ebp]
+%endmacro
+
 %macro IND 1
  %if %1!=dx
  mov dx, %1
  %endif
  in eax, dx
+%endmacro
+
+%macro BASE_IND 2
+ mov dx, word [%1]
+ add dx, %2
+ in eax, dx
+%endmacro
+
+%macro MMIO_IND 2
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov eax, dword [ebp]
 %endmacro
 
 %macro OUTB 2
@@ -41,6 +80,21 @@
  out dx, al
 %endmacro
 
+%macro BASE_OUTB 3
+ mov dx, word [%1]
+ add dx, %2
+ %if %3!=al
+ mov al, %3
+ %endif
+ out dx, al
+%endmacro
+
+%macro MMIO_OUTB 3
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov byte [ebp], %3
+%endmacro
+
 %macro OUTW 2
  %if %1!=dx
  mov dx, %1
@@ -51,6 +105,21 @@
  out dx, ax
 %endmacro
 
+%macro BASE_OUTW 3
+ mov dx, word [%1]
+ add dx, %2
+ %if %3!=ax
+ mov ax, %3
+ %endif
+ out dx, ax
+%endmacro
+
+%macro MMIO_OUTW 3
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov word [ebp], %3
+%endmacro
+
 %macro OUTD 2
  %if %1!=dx
  mov dx, %1
@@ -59,6 +128,21 @@
  mov eax, %2
  %endif
  out dx, eax
+%endmacro
+
+%macro BASE_OUTD 3
+ mov dx, word [%1]
+ add dx, %2
+ %if %3!=eax
+ mov eax, %3
+ %endif
+ out dx, eax
+%endmacro
+
+%macro MMIO_OUTD 3
+ mov ebp, dword [%1]
+ add ebp, %2
+ mov dword [ebp], %3
 %endmacro
 
 %macro HALT 0
