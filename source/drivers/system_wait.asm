e8eefadf3685dec59cc237891a97076ad3039c85
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-07-24 18:02:23 +0200

    Update system_wait.asm

--- source/drivers/system_wait.asm
+++ source/drivers/system_wait.asm
@@ -4,14 +4,19 @@ ticks dd 0
 al_save_state db 0
 
 %macro WAIT 1
- mov eax, %1+1
+ %if (%1 & 0x1)==0x0
+ mov eax, %1/2+1
+ %endif
+ %if (%1 & 0x1)==0x1
+ mov eax, %1/2+2
+ %endif
  call wait_pit
 %endmacro
 
 set_pit:
  OUTB 0x43, 0x36
- OUTB 0x40, 1024 & 0xFF
- OUTB 0x40, 1024 >> 8
+ OUTB 0x40, 2048 & 0xFF
+ OUTB 0x40, 2048 >> 8
 
  ret
 
