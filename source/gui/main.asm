5b3d9a2ade78670665f27abb2e4056a8f49b1d25
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-07-29 20:16:45 +0200

    Add files via upload

--- source/gui/main.asm
+++ source/gui/main.asm
@@ -21,9 +21,8 @@ main_window:
  ;text
  PRINT 'Welcome in BleskOS', main_up_str, LINE(2), COLUMN(2)
  PRINT 'You can open programs with keyboard:', main_second_str, LINE(4), COLUMN(2)
- PRINT '[F1] Developer zone', main_te_str, LINE(6), COLUMN(2)
- SCREEN_Y_SUB eax, LINESZ*3
- PRINT 'You can shutdown computer with press power button', main_down_str, eax, COLUMN(2)
+ PRINT '[F1] Hardware', main_hardware_str, LINE(6), COLUMN(2)
+ PRINT '[F2] Developer zone', main_te_str, LINE(8), COLUMN(2)
 
  call redraw_screen
 
@@ -31,5 +30,8 @@ main_window:
   call wait_for_keyboard
 
   cmp byte [key_code], KEY_F1
+  je hardware_zone
+
+  cmp byte [key_code], KEY_F2
   je developer_zone
  jmp .main_gui_halt
