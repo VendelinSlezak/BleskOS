6d6becbfde3149cac3fe68c149ecb170f36a1189
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-02 18:13:01 +0200

    Update hardware_ps2_keyboard.asm

--- source/drivers/hardware_ps2_keyboard.asm
+++ source/drivers/hardware_ps2_keyboard.asm
@@ -15,10 +15,18 @@
 %define KEY_LEFT 0x4B
 %define KEY_RIGHT 0x4D
 
-%define KEY_F1 0x3B
-%define KEY_F2 0x3C
-%define KEY_F3 0x3D
-%define KEY_F4 0x3E
+#define KEY_F1 0x3B
+#define KEY_F2 0x3C
+#define KEY_F3 0x3D
+#define KEY_F4 0x3E
+#define KEY_F5 0x3F
+#define KEY_F6 0x40
+#define KEY_F7 0x41
+#define KEY_F8 0x42
+#define KEY_F9 0x43
+#define KEY_F10 0x44
+#define KEY_F11 0x57
+#define KEY_F12 0x58
 
 %define KEY_A 0x1E
 %define KEY_B 0x30
