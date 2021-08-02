0aa7020f8dfc4fa05dce3322aa587eeceb054c66
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-02 21:32:16 +0200

    Update hardware_ps2_mouse.asm

--- source/drivers/hardware_ps2_mouse.asm
+++ source/drivers/hardware_ps2_mouse.asm
@@ -2,10 +2,10 @@
 
 ;Touchpad is emulating PS/2 mouse
 
-ps2_mouse_packet_bytes dd 0
-ps2_mouse_data_pointer dd 0
+ps2_mouse_data_pointer dd ps2_mouse_data
 ps2_mouse_data dd 0
 ps2_mouse_wait dd 0
+ps2_mouse_ack dd 0
 
 %macro WRITE_PS2_MOUSE 1
  mov byte [ps2_command], 0xD4
@@ -19,17 +19,18 @@ ps2_mouse_wait dd 0
 %endmacro
 
 enable_touchpad:
- mov dword [ps2_mouse_packet_bytes], 3
-
  ;enable sending packets
+ mov dword [ps2_mouse_ack], 1
  WRITE_PS2_MOUSE 0xF4
  READ_PS2_MOUSE
- mov dword [ps2_mouse_data_pointer], 0
+ mov dword [ps2_mouse_data_pointer], ps2_mouse_data
+ mov dword [ps2_mouse_ack], 0
 
  ret
 
 disable_touchpad:
  ;disable sending packets
+ mov dword [ps2_mouse_ack], 1
  WRITE_PS2_MOUSE 0xF5
  READ_PS2_MOUSE
 
@@ -38,42 +39,25 @@ disable_touchpad:
 ps2_mouse_irq:
  pusha
 
- mov eax, 0
- INB 0x60
+ in al, 0x60
 
  ;ACK
- cmp al, 0xFA
+ cmp dword [ps2_mouse_ack], 1
  je .done
 
  ;save data
- mov ebx, ps2_mouse_data
- add ebx, dword [ps2_mouse_data_pointer]
+ mov ebx, dword [ps2_mouse_data_pointer]
  mov byte [ebx], al
  inc dword [ps2_mouse_data_pointer]
 
  ;packet is received
- cmp dword [ps2_mouse_data_pointer], 3
+ cmp dword [ps2_mouse_data_pointer], ps2_mouse_data+3
  jl .done
 
- mov dword [ps2_mouse_data_pointer], 0
+ mov dword [ps2_mouse_data_pointer], ps2_mouse_data
  mov dword [ps2_mouse_wait], 0
 
  .done:
  EOI_SLAVE_PIC
  popa
  iret
-
-wait_for_ps2_mouse_or_keyboard:
- mov dword [ps2_mouse_wait], 1
- mov dword [keyboard_wait], 1
-
- .wait:
-  cmp dword [ps2_mouse_wait], 0
-  je .done
-  cmp dword [keyboard_wait], 0
-  je .done
-  hlt
- jmp .wait
-
- .done:
- ret
