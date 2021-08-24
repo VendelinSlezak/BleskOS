ce719865326e0bc3ec1dc07f8f3c8748333d6a52
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-24 18:54:30 +0200

    Update sound_hda.asm

--- source/drivers/sound_hda.asm
+++ source/drivers/sound_hda.asm
@@ -146,15 +146,21 @@
  mov dword [MEMORY_HDA_BUFFER+0x1C], 0
 %endmacro
 
+%define HDA_PIO_INTERFACE 1
+%define HDA_CORB_RIRB_INTERFACE 2
+
 hda_base dd 0
 hda_input_stream_port dd 0
 hda_output_stream_port dd 0
 
+verb_interface dd 0
 verb_codec dd 0
 verb_node dd 0
 verb_verb dd 0
 verb_command dd 0
 hda_response dd 0
+corb_pointer db 0
+rirb_pointer db 0
 
 hda_codec_id dd 0
 hda_max_volume dd 0
@@ -208,13 +214,42 @@ init_sound_card:
  HDA_OUTPUT_STREAM_SET_BUFFER
  HDA_SET_SSYNC
 
+ ;set CORB and RIRB
+ MMIO_OUTB hda_base, 0x4C, 0x0 ;stop CORB
+ MMIO_OUTB hda_base, 0x5C, 0x0 ;stop RIRB
+ WAIT 10
+ MMIO_OUTD hda_base, 0x40, MEMORY_CORB
+ MMIO_OUTD hda_base, 0x44, 0x00000000
+ MMIO_OUTB hda_base, 0x4E, 0x2 ;size 256 entries
+ MMIO_OUTW hda_base, 0x4A, 0x8000 ;reset read pointer
+ WAIT 10
+ MMIO_OUTW hda_base, 0x4A, 0x0000
+ WAIT 10
+ MMIO_OUTW hda_base, 0x48, 0 ;write pointer
+ mov byte [corb_pointer], 1
+
+ MMIO_OUTD hda_base, 0x50, MEMORY_RIRB
+ MMIO_OUTD hda_base, 0x54, 0x00000000
+ MMIO_OUTB hda_base, 0x5E, 0x2 ;size 256 entries
+ MMIO_OUTW hda_base, 0x58, 0x8000 ;reset write pointer
+ WAIT 10
+ MMIO_OUTW hda_base, 0x58, 0x0000
+ WAIT 10
+ MMIO_OUTW hda_base, 0x5A, 0x0 ;interrupt control
+ mov byte [rirb_pointer], 1
+
+ MMIO_OUTB hda_base, 0x5C, 0x2 ;start RIRB
+ MMIO_OUTB hda_base, 0x4C, 0x2 ;start CORB
+
  ;FIND CODEC
+ mov dword [verb_interface], HDA_PIO_INTERFACE
  mov dword [verb_codec], 0
  mov dword [verb_node], 0
  mov dword [verb_verb], 0xF00
  mov dword [verb_command], 0x00
  mov ecx, 16
  .find_codec:
+ push ecx
   call hda_send_verb
   cmp dword [hda_response], 0x00000000
   je .next_codec
@@ -223,14 +258,39 @@ init_sound_card:
 
   mov eax, dword [hda_response]
   mov dword [hda_codec_id], eax
-  jmp codec_find_widgets
+  jmp .codec
  .next_codec:
+ pop ecx
  inc dword [verb_codec]
  loop .find_codec
+
+ mov dword [verb_interface], HDA_CORB_RIRB_INTERFACE
+ mov dword [verb_codec], 0
+ mov dword [verb_node], 0
+ mov dword [verb_verb], 0xF00
+ mov dword [verb_command], 0x00
+ mov ecx, 16
+ .find_codec_2:
+ push ecx
+  call hda_send_verb
+  cmp dword [hda_response], 0x00000000
+  je .next_codec_2
+  cmp dword [hda_response], 0xFFFFFFFF
+  je .next_codec_2
+
+  mov eax, dword [hda_response]
+  mov dword [hda_codec_id], eax
+  jmp .codec
+ .next_codec_2:
+ pop ecx
+ inc dword [verb_codec]
+ loop .find_codec_2
 
  .done:
  ret ;no codec founded
 
+ .codec:
+ pop ecx
 codec_find_widgets:
  mov dword [hda_line_out_node], 0
  mov dword [hda_line_in_node], 0
@@ -299,31 +359,60 @@ codec_find_widgets:
  cmp ecx, 0
  jne .scan_node
 
- ;TURN ON FIRST SPEAKER
- mov esi, hda_nodes_list
- mov ecx, 128
- .find_first_speaker:
-  cmp byte [esi+1], 0x10
-  jne .next_loop
-
-  mov eax, 0
-  mov al, byte [esi]
-  mov dword [hda_speaker_node], eax
-  jmp .enable_speaker
-
- .next_loop:
- add esi, 2
- loop .find_first_speaker
-
- .enable_speaker:
- mov eax, dword [hda_speaker_node]
- mov dword [verb_node], eax
- call hda_enable_output_pin
-
  .done:
  ret
 
 hda_send_verb:
+ cmp dword [verb_interface], HDA_PIO_INTERFACE
+ je .pio
+
+ call hda_send_verb_corb
+ ret
+
+ .pio:
+ call hda_send_verb_pio
+ ret
+
+hda_send_verb_corb:
+ mov eax, 0
+ mov ebx, dword [verb_codec]
+ shl ebx, 28
+ or eax, ebx
+ mov ebx, dword [verb_node]
+ shl ebx, 20
+ or eax, ebx
+ mov ebx, dword [verb_verb]
+ shl ebx, 8
+ or eax, ebx
+ or eax, dword [verb_command]
+
+ mov ebx, 0
+ mov bl, byte [corb_pointer]
+ mov dword [MEMORY_CORB+(ebx*4)], eax ;write verb to CORB
+
+ mov ebx, 0
+ mov bl, byte [corb_pointer]
+ MMIO_OUTW hda_base, 0x48, bx ;update pointer
+
+ mov dword [ticks], 0
+ .wait:
+  MMIO_INW hda_base, 0x58
+  cmp al, bl
+  je .done
+ cmp dword [ticks], 5
+ jl .wait
+
+ .done:
+ mov ebx, 0
+ mov bl, byte [rirb_pointer]
+ mov eax, dword [MEMORY_RIRB+(ebx*8)]
+ mov dword [hda_response], eax
+ inc byte [corb_pointer]
+ inc byte [rirb_pointer]
+
+ ret
+
+hda_send_verb_pio:
  mov eax, 0
 
  mov ebx, dword [verb_codec]
@@ -371,12 +460,14 @@ hda_send_verb:
  ret
 
 hda_set_volume:
- cmp dword [hda_volume], 0
- je .mute
-
  mov dword [verb_verb], 0xF00
  mov dword [verb_command], 0x12 ;get amplifier info
  call hda_send_verb
+
+ mov eax, dword [hda_response]
+ and eax, 0xFF00
+ cmp eax, 0
+ je .done ;volume can not be changed
 
  ;calculate from range 0 to 100 to node range
  mov eax, dword [hda_response]
@@ -391,24 +482,32 @@ hda_set_volume:
  mov dword [verb_command], eax
  call hda_send_verb
 
- ret
-
- .mute:
- mov dword [verb_verb], 0x300
- mov dword [verb_command], 0xB080
- call hda_send_verb
+ .done:
  ret
 
 hda_enable_audio_output:
+ ;unmute output
  mov dword [hda_volume], 100
  call hda_set_volume
 
+ ;set stream 1
  mov dword [verb_verb], 0x706
- mov dword [verb_command], 0x10 ;stream 1
+ mov dword [verb_command], 0x10
  call hda_send_verb
 
+ ;set data format
  mov dword [verb_verb], 0x200
  mov eax, dword [hda_data_format]
+ mov dword [verb_command], eax
+ call hda_send_verb
+
+ ;turn EAPD on
+ mov dword [verb_verb], 0xF0C
+ mov dword [verb_command], 0x00
+ call hda_send_verb
+ mov eax, dword [hda_response]
+ or eax, 0x2
+ mov dword [verb_verb], 0x70C
  mov dword [verb_command], eax
  call hda_send_verb
 
@@ -418,9 +517,11 @@ hda_enable_output_pin:
  cmp dword [verb_node], 0
  je .done
 
+ ;unmute pin
  mov dword [hda_volume], 100
  call hda_set_volume
 
+ ;enable output pin
  mov dword [verb_verb], 0xF07
  mov dword [verb_command], 0x00
  call hda_send_verb
@@ -430,19 +531,20 @@ hda_enable_output_pin:
  mov dword [verb_command], eax
  call hda_send_verb
 
+ ;turn EAPD on
  mov dword [verb_verb], 0xF0C
  mov dword [verb_command], 0x00
  call hda_send_verb
  mov eax, dword [hda_response]
- or eax, 0x2 ;turn EAPD on
+ or eax, 0x2
  mov dword [verb_verb], 0x70C
  mov dword [verb_command], eax
  call hda_send_verb
 
+ ;get connection entry
  mov dword [verb_verb], 0xF02
  mov dword [verb_command], 0x0
  call hda_send_verb
-
  mov eax, dword [hda_response]
  and eax, 0xFF
  mov dword [verb_node], eax
@@ -452,6 +554,7 @@ hda_enable_output_pin:
  mov eax, dword [hda_response]
  shr eax, 20
 
+ ;if it is mixer, unmute it
  cmp eax, 0x4
  jne .if_mixer
   mov dword [hda_volume], 100
