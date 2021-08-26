b6870fc95ee34b570f0e4a7fb86476c2a1365f9f
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-26 16:01:20 +0200

    Update usb_mouse.asm

--- source/drivers/usb_mouse.asm
+++ source/drivers/usb_mouse.asm
@@ -8,7 +8,6 @@ usb_mouse_speed dd 0
 usb_mouse_endpoint dd 0
 
 usb_mouse_data dd 0
-usb_mouse_wait dd 0
 
 usb_mouse_uhci_remove:
  mov ax, word [usb_mouse_base]
@@ -30,6 +29,8 @@ usb_mouse_uhci_remove:
 read_usb_mouse:
  cmp dword [usb_mouse_controller], UHCI
  je .uhci
+ cmp dword [usb_mouse_controller], OHCI
+ je .ohci
  ret
 
  .uhci:
@@ -54,6 +55,22 @@ read_usb_mouse:
 
  ret
 
+ .ohci:
+ mov ax, word [usb_mouse_base]
+ mov word [ohci_base], ax
+ mov eax, dword [usb_mouse_speed]
+ mov dword [ohci_device_speed], eax
+ mov eax, dword [usb_mouse_endpoint]
+ mov dword [ohci_endpoint], eax
+
+ mov dword [MEMORY_OHCI+0x300], 0
+ mov dword [MEMORY_OHCI+0x300+4], 0
+ call ohci_read_hid
+ mov eax, dword [MEMORY_OHCI+0x300]
+ mov dword [usb_mouse_data], eax
+
+ ret
+
  .usb_mouse_3_bytes:
  mov eax, dword [MEMORY_UHCI+0x10200]
  and eax, 0x00FFFFFF
@@ -75,10 +92,11 @@ read_usb_mouse:
  ret
 
 wait_for_usb_mouse:
- mov dword [usb_mouse_wait], 0
  mov dword [ps2_mouse_wait], 1 ;PS/2 mouse
  mov dword [ps2_mouse_data_pointer], ps2_mouse_data
  mov dword [keyboard_wait], 1 ;PS/2 keyboard
+ mov dword [usb_keyboard_wait], 0
+ mov dword [ticks], 0
 
  .wait:
   hlt
@@ -89,14 +107,22 @@ wait_for_usb_mouse:
   cmp dword [ps2_mouse_wait], 0
   je .ps2_mouse
 
-  inc dword [usb_mouse_wait]
-  cmp dword [usb_mouse_wait], 8
+  cmp dword [ticks], 4
   jl .wait
   mov dword [usb_mouse_data], 0
   call read_usb_mouse
   cmp dword [usb_mouse_data], 0x0
   jne .done
-  mov dword [usb_mouse_wait], 0
+  mov dword [ticks], 0
+
+  inc dword [usb_keyboard_wait]
+  cmp dword [usb_keyboard_wait], 2
+  jl .wait
+  mov dword [usb_keyboard_data], 0
+  call read_usb_keyboard
+  cmp dword [key_code], 0x0
+  jne .done
+  mov dword [usb_keyboard_wait], 0
  jmp .wait
 
  .ps2_mouse:
