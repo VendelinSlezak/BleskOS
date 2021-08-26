94f4dcaa805df18e8b3cd879ef6d6f12bc0b9b2e
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-26 14:12:47 +0200

    Update usb_uhci.asm

--- source/drivers/usb_uhci.asm
+++ source/drivers/usb_uhci.asm
@@ -1,72 +1,67 @@
 ;BleskOS
 
-%define UHCI_LOW_SPEED 0x04800000
-%define UHCI_FULL_SPEED 0x00800000
-
-%macro UHCI_CLEAR_FRAME_LIST 0
+%macro UHCI_FILL_FRAME_LIST 1
  mov eax, dword [uhci_controller_number]
  mov ebx, 4096
  mul ebx
- mov esi, MEMORY_UHCI
- add esi, eax
+ add eax, MEMORY_UHCI
+
+ mov edi, eax
+ mov eax, %1
  mov ecx, 1024
- .clear_list:
-  mov dword [esi], 0x1 ;invalid pointer
-  add esi, 4
- loop .clear_list
+ rep stosd
 %endmacro
 
-%macro UHCI_FILL_FRAME_LIST 0
- mov eax, dword [uhci_controller_number]
- mov ebx, 4096
- mul ebx
- mov esi, MEMORY_UHCI
- add esi, eax
- mov ecx, 1024
- .fill_list:
-  mov dword [esi], MEMORY_UHCI+0x10002 ;pointer
-  add esi, 4
- loop .fill_list
+%macro UHCI_CREATE_QH 0
+ mov dword [MEMORY_UHCI+0x10000], 0x1
+ mov dword [MEMORY_UHCI+0x10004], MEMORY_UHCI+0x10100
 %endmacro
 
-%macro UHCI_CREATE_QH 2
- mov dword [MEMORY_UHCI+0x10000], %1
- mov dword [MEMORY_UHCI+0x10000+4], %2
-%endmacro
-
-%macro UHCI_CREATE_TD 4
- mov dword [%1], %2
+%macro UHCI_CREATE_TD_ZERO_DEVICE 4
+ mov dword [%1+0], %2
  mov eax, dword [uhci_device_speed]
  mov dword [%1+4], eax
  mov dword [%1+8], %3
  mov dword [%1+12], %4
 %endmacro
 
-%macro UHCI_SETUP_TRANSFER 1
- UHCI_CREATE_QH 0x1, MEMORY_UHCI+0x10100
- UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10200 | 0x4, 0x00E0012D, %1 ;setup
- UHCI_CREATE_TD MEMORY_UHCI+0x10200, 0x1, 0xFFE80169, MEMORY_UHCI+0x10300+8 ;in
- mov dword [uhci_td_pointer], MEMORY_UHCI+0x10200+4
- mov dword [uhci_time_for_transfer], 100
- call uhci_transfer_queue_head
+%macro UHCI_CREATE_TD 4
+ mov dword [%1+0], %2
+ mov eax, dword [uhci_device_speed]
+ mov dword [%1+4], eax
+ mov ebx, 0
+ mov bh, byte [uhci_address]
+ or ebx, %3
+ mov dword [%1+8], ebx
+ mov dword [%1+12], %4
 %endmacro
 
+%define UHCI_LOW_SPEED 0x04800000
+%define UHCI_FULL_SPEED 0x00800000
+%define UHCI_SETUP 0x2D
+%define UHCI_IN 0x69
+%define UHCI_OUT 0xE1
+%define UHCI_DATA_TOGGLE_0 0x00000000
+%define UHCI_DATA_TOGGLE_1 0x00080000
+%define UHCI_TRANSFER_8_BYTES 0x00E00000
+%define UHCI_TRANSFER_0_BYTES 0xFFE00000
+
 uhci_base dw 0
+uhci_port_base dw 0
+uhci_address db 0
 uhci_controller_number dd 0
+uhci_conf_number dd 0
+uhci_interface_number dd 0
+uhci_alt_interface_number dd 0
+
 uhci_device_speed dd 0
-uhci_toggle dd 0
 uhci_endpoint dd 0
-uhci_td_pointer dd 0
-uhci_time_for_transfer dd 0
+uhci_td dd 0
+uhci_wait dd 0
 
-uhci_conf_num db 0
-uhci_interface_num db 0
-uhci_alt_interface_num db 0
+uhci_reset_every_device dd 0
 
-init_uhci_controller:
- cmp word [uhci_base], 0
- je .done
-
+init_uhci:
  ;reset controller
  BASE_OUTW uhci_base, 0x0, 0x4
  WAIT 10
@@ -84,188 +79,165 @@ init_uhci_controller:
  BASE_OUTD uhci_base, 0x8, eax ;set frame address pointer
 
  ;clear frame list
- UHCI_CLEAR_FRAME_LIST
+ UHCI_FILL_FRAME_LIST 0x1
  BASE_OUTW uhci_base, 0x0, 0x1 ;run controller
 
- .done:
  ret
 
 uhci_detect_devices:
- ;detect device on first port
- BASE_INW uhci_base, 0x10
- and ax, 0x3
- cmp ax, 0x0 ;no device
- je .no_device_first_port
- cmp ax, 0x2 ;no device
- je .no_device_first_port
- cmp ax, 0x1 ;initalized device
- je .done
+ mov ax, word [uhci_base]
+ add ax, 0x10
+ mov word [uhci_port_base], ax
+ mov byte [uhci_address], 1
 
- ;initalize device
- call usb_mouse_uhci_remove
- BASE_OUTW uhci_base, 0x10, 0x200 ;reset
- WAIT 50
- BASE_OUTW uhci_base, 0x10, 0x0
- WAIT 10
- BASE_INW uhci_base, 0x10
- mov dword [uhci_device_speed], UHCI_FULL_SPEED
- and ax, 0x100
- cmp ax, 0x100
- jne .if_low_speed
-  mov dword [uhci_device_speed], UHCI_LOW_SPEED
- .if_low_speed:
- BASE_OUTW uhci_base, 0x10, 0x6 ;enable device and clear status change
- WAIT 50
- call uhci_init_device
- jmp .done
+ mov ecx, 10 ;max ten ports
+ .detect_device:
+  BASE_INW uhci_port_base, 0
+  cmp ax, 0xFFFF
+  je .done
+  test ax, 0x80
+  jz .done
+  BASE_INW uhci_port_base, 0
 
- .no_device_first_port:
- mov ax, word [usb_mouse_base]
- cmp word [uhci_base], ax
- jne .detect_second_port
- call usb_mouse_uhci_remove
+  push ecx
+  call uhci_detect_device
+  pop ecx
 
- ;detect device on second port
- .detect_second_port:
- BASE_INW uhci_base, 0x12
- and ax, 0x3
- cmp ax, 0x0 ;no device
- je .no_device
- cmp ax, 0x2 ;no device
- je .done
- cmp ax, 0x1 ;initalized device
- je .done
-
- ;initalize device
- call usb_mouse_uhci_remove
- BASE_OUTW uhci_base, 0x12, 0x200 ;reset
- WAIT 50
- BASE_OUTW uhci_base, 0x12, 0x0
- WAIT 10
- BASE_INW uhci_base, 0x12
- mov dword [uhci_device_speed], UHCI_FULL_SPEED
- and ax, 0x100
- cmp ax, 0x100
- jne .if_low_speed_2
-  mov dword [uhci_device_speed], UHCI_LOW_SPEED
- .if_low_speed_2:
- BASE_OUTW uhci_base, 0x12, 0x6 ;enable device and clear status change
- WAIT 50
- call uhci_init_device
- jmp .done
-
- .no_device:
- mov ax, word [usb_mouse_base]
- cmp word [uhci_base], ax
- jne .done
- call usb_mouse_uhci_remove
+  add word [uhci_port_base], 0x2
+  inc byte [uhci_address]
+ loop .detect_device
 
  .done:
  ret
 
-uhci_init_device:
- ;SET ADDRESS
- UHCI_SETUP_TRANSFER MEMORY_UHCI+0x10300
- UHCI_CREATE_QH 0x1, MEMORY_UHCI+0x10100
- UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10200 | 0x4, 0x00E0002D, MEMORY_UHCI+0x10300 ;setup
- UHCI_CREATE_TD MEMORY_UHCI+0x10200, 0x1, 0xFFE80069, MEMORY_UHCI+0x10400 ;in
- mov dword [MEMORY_UHCI+0x10300], 0x00010500
- mov dword [MEMORY_UHCI+0x10300+4], 0x00000000
- mov dword [uhci_td_pointer], MEMORY_UHCI+0x10200+4
- mov dword [uhci_time_for_transfer], 100
+uhci_detect_device:
+ ;is device connected?
+ BASE_INW uhci_port_base, 0
+ test ax, 0x1
+ jz .no_device
+
+ cmp dword [uhci_reset_every_device], 1
+ je .initalize_device
+
+ ;is device initalized?
+ BASE_INW uhci_port_base, 0
+ test ax, 0x2
+ jz .initalized_device
+
+ .initalize_device:
+ call usb_mouse_uhci_remove
+ BASE_OUTW uhci_port_base, 0, 0x200 ;reset device
+ WAIT 50
+ BASE_OUTW uhci_port_base, 0, 0x0
+ WAIT 30
+
+ BASE_INW uhci_port_base, 0
+ mov dword [uhci_device_speed], UHCI_FULL_SPEED
+ test ax, 0x100
+ jz .if_low_speed
+  mov dword [uhci_device_speed], UHCI_LOW_SPEED
+ .if_low_speed:
+
+ BASE_OUTW uhci_port_base, 0, 0x6 ;enable device and clear status change
+ WAIT 50
+ BASE_INW uhci_port_base, 0
+ test ax, 0x4
+ jz .no_device ;device is not enabled
+
+ call uhci_set_address
+ call uhci_read_descriptor
+ ret
+
+ .no_device:
+ call usb_mouse_uhci_remove
+ .initalized_device:
+ ret
+
+uhci_set_address:
+ UHCI_CREATE_QH
+ UHCI_CREATE_TD_ZERO_DEVICE MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_SETUP), MEMORY_UHCI+0x10200
+ UHCI_CREATE_TD_ZERO_DEVICE MEMORY_UHCI+0x10110, 0x1, (UHCI_TRANSFER_0_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), 0x0
+ mov dword [MEMORY_UHCI+0x10200], 0x00000500
+ mov dword [MEMORY_UHCI+0x10204], 0x00000000
+ mov al, byte [uhci_address]
+ mov byte [MEMORY_UHCI+0x10202], al
+
+ mov dword [uhci_td], MEMORY_UHCI+0x10110+4
+ mov dword [uhci_wait], 100
  call uhci_transfer_queue_head
 
- ;GET DESCRIPTOR
- UHCI_CREATE_QH 0x1, MEMORY_UHCI+0x10100
- UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10200 | 0x4, 0x00E0012D, MEMORY_UHCI+0x10B00 ;setup
- UHCI_CREATE_TD MEMORY_UHCI+0x10200, MEMORY_UHCI+0x10300 | 0x4, 0x00E80169, MEMORY_UHCI+0x10C00+0 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10300, MEMORY_UHCI+0x10400 | 0x4, 0x00E00169, MEMORY_UHCI+0x10C00+8 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10400, MEMORY_UHCI+0x10500 | 0x4, 0x00E80169, MEMORY_UHCI+0x10C00+16 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10500, MEMORY_UHCI+0x10600 | 0x4, 0x00E00169, MEMORY_UHCI+0x10C00+24 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10600, MEMORY_UHCI+0x10700 | 0x4, 0x00E80169, MEMORY_UHCI+0x10C00+32 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10700, MEMORY_UHCI+0x10800 | 0x4, 0x00E00169, MEMORY_UHCI+0x10C00+40 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10800, MEMORY_UHCI+0x10900 | 0x4, 0x00E80169, MEMORY_UHCI+0x10C00+48 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10900, MEMORY_UHCI+0x10A00 | 0x4, 0x00E00169, MEMORY_UHCI+0x10C00+56 ;in
- UHCI_CREATE_TD MEMORY_UHCI+0x10A00, 0x1, 0xFFE801E1, MEMORY_UHCI+0x10700+24 ;out
- mov dword [MEMORY_UHCI+0x10B00], 0x02000680
- mov dword [MEMORY_UHCI+0x10B00+4], 0x00400000
- mov dword [uhci_td_pointer], MEMORY_UHCI+0x10A00+4
- mov dword [uhci_time_for_transfer], 100
+ ret
+
+uhci_read_descriptor:
+ UHCI_CREATE_QH
+ UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_SETUP), MEMORY_UHCI+0x10200
+ UHCI_CREATE_TD MEMORY_UHCI+0x10110, MEMORY_UHCI+0x10120 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), MEMORY_UHCI+0x10300
+ UHCI_CREATE_TD MEMORY_UHCI+0x10120, MEMORY_UHCI+0x10130 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_IN), MEMORY_UHCI+0x10300+8
+ UHCI_CREATE_TD MEMORY_UHCI+0x10130, MEMORY_UHCI+0x10140 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), MEMORY_UHCI+0x10300+16
+ UHCI_CREATE_TD MEMORY_UHCI+0x10140, MEMORY_UHCI+0x10150 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_IN), MEMORY_UHCI+0x10300+24
+ UHCI_CREATE_TD MEMORY_UHCI+0x10150, MEMORY_UHCI+0x10160 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), MEMORY_UHCI+0x10300+32
+ UHCI_CREATE_TD MEMORY_UHCI+0x10160, MEMORY_UHCI+0x10170 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_IN), MEMORY_UHCI+0x10300+40
+ UHCI_CREATE_TD MEMORY_UHCI+0x10170, MEMORY_UHCI+0x10180 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), MEMORY_UHCI+0x10300+48
+ UHCI_CREATE_TD MEMORY_UHCI+0x10180, 0x1, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_IN), MEMORY_UHCI+0x10300+56
+ mov dword [MEMORY_UHCI+0x10200], 0x02000680
+ mov dword [MEMORY_UHCI+0x10204], 0x00400000
+
+ mov dword [uhci_td], MEMORY_UHCI+0x10180+4
+ mov dword [uhci_wait], 100
  call uhci_transfer_queue_head
 
- ;PARSE RECEIVED DATA
- mov esi, MEMORY_UHCI+0x10C00
+ ;parse descriptor
+ mov esi, MEMORY_UHCI+0x10300
  call parse_usb_descriptor
 
  mov al, byte [usb_descriptor+20]
- mov byte [uhci_conf_num], al
+ mov byte [uhci_conf_number], al
  mov al, byte [usb_descriptor+21]
- mov byte [uhci_interface_num], al
+ mov byte [uhci_interface_number], al
  mov al, byte [usb_descriptor+22]
- mov byte [uhci_alt_interface_num], al
+ mov byte [uhci_alt_interface_number], al
  cmp dword [usb_descriptor+23], 0x00020103
  je .usb_mouse_2
 
  mov al, byte [usb_descriptor+10]
- mov byte [uhci_conf_num], al
+ mov byte [uhci_conf_number], al
  mov al, byte [usb_descriptor+11]
- mov byte [uhci_interface_num], al
+ mov byte [uhci_interface_number], al
  mov al, byte [usb_descriptor+12]
- mov byte [uhci_alt_interface_num], al
+ mov byte [uhci_alt_interface_number], al
  cmp dword [usb_descriptor+13], 0x00010103
  je .usb_keyboard
  cmp dword [usb_descriptor+13], 0x00020103
  je .usb_mouse
-
  ret
 
  .usb_keyboard:
- jmp .done
+ ret
 
  .usb_mouse:
- ;SET CONFIGURATION
- mov dword [MEMORY_UHCI+0x10300], 0x00000900
- mov dword [MEMORY_UHCI+0x10300+4], 0x00000000
- mov al, byte [uhci_conf_num]
- mov byte [MEMORY_UHCI+0x10300+2], al
- UHCI_SETUP_TRANSFER MEMORY_UHCI+0x10300
-
- ;SET INTERFACE
- mov dword [MEMORY_UHCI+0x10300], 0x00000B01
- mov dword [MEMORY_UHCI+0x10300+4], 0x00000000
- mov al, byte [uhci_interface_num]
- mov byte [MEMORY_UHCI+0x10300+4], al
- mov al, byte [uhci_alt_interface_num]
- mov byte [MEMORY_UHCI+0x10300+2], al
- UHCI_SETUP_TRANSFER MEMORY_UHCI+0x10300
-
- ;SET IDLE - needed for some mouses
- mov dword [MEMORY_UHCI+0x10300], 0x00000A21
- mov dword [MEMORY_UHCI+0x10300+4], 0x00000000
- UHCI_SETUP_TRANSFER MEMORY_UHCI+0x10300
+ call uhci_set_configuration
+ call uhci_set_interface
+ call uhci_set_idle
 
  ;save mouse
  mov dword [usb_mouse_controller], UHCI
-
  mov ax, word [uhci_base]
  mov word [usb_mouse_base], ax
-
  mov eax, dword [uhci_controller_number]
  mov dword [usb_mouse_controller_number], eax
-
  mov eax, dword [uhci_device_speed]
  mov dword [usb_mouse_speed], eax
-
  mov eax, 0
  mov al, byte [usb_descriptor+19]
  mov dword [usb_mouse_endpoint], eax
+ PVAR eax
+ mov al, byte [uhci_address]
+ mov byte [usb_mouse_address], al
 
- .done:
  ret
 
  .usb_mouse_2:
  call .usb_mouse
-
  mov eax, 0
  mov al, byte [usb_descriptor+29]
  mov dword [usb_mouse_endpoint], eax
@@ -273,37 +245,94 @@ uhci_init_device:
 
  ret
 
-uhci_read_hid:
- UHCI_CREATE_QH 0x1, MEMORY_UHCI+0x10100
+uhci_set_configuration:
+ UHCI_CREATE_QH
+ UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_SETUP), MEMORY_UHCI+0x10200
+ UHCI_CREATE_TD MEMORY_UHCI+0x10110, 0x1, (UHCI_TRANSFER_0_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), 0x0
+ mov dword [MEMORY_UHCI+0x10200], 0x00000900
+ mov dword [MEMORY_UHCI+0x10204], 0x00000000
+ mov al, byte [uhci_conf_number]
+ mov byte [MEMORY_UHCI+0x10202], al
 
- mov ebx, dword [uhci_endpoint]
- shl ebx, 15
- or ebx, 0x00E00169
- UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10200, ebx, MEMORY_UHCI+0x10300
- UHCI_CREATE_TD MEMORY_UHCI+0x10200, 0x1, ebx, MEMORY_UHCI+0x10300+8
-
- mov dword [uhci_td_pointer], MEMORY_UHCI+0x10200+4
- mov dword [uhci_time_for_transfer], 3
+ mov dword [uhci_td], MEMORY_UHCI+0x10110+4
+ mov dword [uhci_wait], 100
  call uhci_transfer_queue_head
 
  ret
 
+uhci_set_interface:
+ UHCI_CREATE_QH
+ UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_SETUP), MEMORY_UHCI+0x10200
+ UHCI_CREATE_TD MEMORY_UHCI+0x10110, 0x1, (UHCI_TRANSFER_0_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), 0x0
+ mov dword [MEMORY_UHCI+0x10200], 0x00000B01
+ mov dword [MEMORY_UHCI+0x10204], 0x00000000
+ mov al, byte [uhci_interface_number]
+ mov byte [MEMORY_UHCI+0x10204], al
+ mov al, byte [uhci_alt_interface_number]
+ mov byte [MEMORY_UHCI+0x10202], al
+
+ mov dword [uhci_td], MEMORY_UHCI+0x10110+4
+ mov dword [uhci_wait], 100
+ call uhci_transfer_queue_head
+
+ ret
+
+uhci_set_idle:
+ UHCI_CREATE_QH
+ UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_SETUP), MEMORY_UHCI+0x10200
+ UHCI_CREATE_TD MEMORY_UHCI+0x10110, 0x1, (UHCI_TRANSFER_0_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN), 0x0
+ mov dword [MEMORY_UHCI+0x10200], 0x00000A21
+ mov dword [MEMORY_UHCI+0x10204], 0x00000000
+ mov al, byte [uhci_interface_number]
+ mov byte [MEMORY_UHCI+0x10204], al
+
+ mov dword [uhci_td], MEMORY_UHCI+0x10110+4
+ mov dword [uhci_wait], 100
+ call uhci_transfer_queue_head
+
+ ret
+
+uhci_read_hid:
+ UHCI_CREATE_QH
+ mov ecx, dword [uhci_endpoint]
+ shl ecx, 15
+ or ecx, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_0 | UHCI_IN)
+ UHCI_CREATE_TD MEMORY_UHCI+0x10100, MEMORY_UHCI+0x10110 | 0x4, ecx, MEMORY_UHCI+0x10200
+ mov ecx, dword [uhci_endpoint]
+ shl ecx, 15
+ or ecx, (UHCI_TRANSFER_8_BYTES | UHCI_DATA_TOGGLE_1 | UHCI_IN)
+ UHCI_CREATE_TD MEMORY_UHCI+0x10110, 0x1, ecx, MEMORY_UHCI+0x10208
+
+ mov dword [uhci_td], MEMORY_UHCI+0x10110+4
+ mov dword [uhci_wait], 3
+ call uhci_transfer_queue_head
+
+ test dword [MEMORY_UHCI+0x10104], 0x007F0000
+ jnz .error
+
+ mov al, byte [MEMORY_UHCI+0x10104]
+ inc al ;number of transferred bytes
+
+ ret
+
+ .error:
+ mov dword [MEMORY_UHCI+0x10200], 0
+ mov dword [MEMORY_UHCI+0x10200+4], 0
+ ret
+
 uhci_transfer_queue_head:
- UHCI_FILL_FRAME_LIST
+ UHCI_FILL_FRAME_LIST MEMORY_UHCI+0x10000 | 2
 
- ;transfer
- mov eax, dword [uhci_time_for_transfer]
- mov ecx, dword [uhci_td_pointer]
+ mov eax, dword [uhci_td]
+ mov ebx, dword [uhci_wait]
  mov dword [ticks], 0
- .wait_for_transfer:
-  mov ebx, dword [ecx]
-  and ebx, 0x00800000
-  cmp ebx, 0
-  je .transfer_is_done
- cmp dword [ticks], eax
- jnge .wait_for_transfer
+ .wait:
+  mov ecx, dword [eax]
+  test ecx, 0x00800000
+  jz .done
+ cmp dword [ticks], ebx
+ jl .wait
 
- .transfer_is_done:
- UHCI_CLEAR_FRAME_LIST
-
+ .done:
+ UHCI_FILL_FRAME_LIST 0x1
  ret
