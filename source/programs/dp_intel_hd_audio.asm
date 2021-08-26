eac5a17e73a0392c47845006829506ac0a82619c
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-26 16:34:03 +0200

    Update dp_intel_hd_audio.asm

--- source/programs/dp_intel_hd_audio.asm
+++ source/programs/dp_intel_hd_audio.asm
@@ -7,46 +7,96 @@ dp_hda:
  ;orange background
  CLEAR_SCREEN DEVELOPER_ZONE_COLOR
 
+ cmp dword [hda_base], 0
+ je .no_hda
+
  ;text
- PRINT 'Line Out:', line_out_str, LINE(1), COLUMN(1)
- mov eax, dword [hda_line_out_node]
- PRINT_VAR eax, LINE(1), COLUMN(11)
+ mov dword [cursor_line], LINE(1)
+ mov dword [color], BLACK
+ mov esi, hda_nodes_list
+ mov ecx, 32
+ .print_values:
+ push ecx
+  mov dword [cursor_column], COLUMN(1)
+  mov eax, 0
+  mov al, byte [esi]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
 
- PRINT 'Line In:', line_in_str, LINE(3), COLUMN(1)
- mov eax, dword [hda_line_in_node]
- PRINT_VAR eax, LINE(3), COLUMN(10)
+  mov dword [cursor_column], COLUMN(12)
+  mov eax, 0
+  mov al, byte [esi+1]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
 
- PRINT 'Speaker:', speaker_str, LINE(5), COLUMN(1)
- mov eax, dword [hda_speaker_node]
- PRINT_VAR eax, LINE(5), COLUMN(10)
+  mov dword [cursor_column], COLUMN(23)
+  mov eax, 0
+  mov al, byte [esi]
+  mov dword [verb_node], eax
+  mov dword [verb_verb], 0xF02
+  mov dword [verb_command], 0x0
+  call hda_send_verb
+  mov eax, dword [hda_response]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
 
- PRINT 'Microphone:', mic_str, LINE(7), COLUMN(1)
- mov eax, dword [hda_mic_node]
- PRINT_VAR eax, LINE(7), COLUMN(13)
+  mov dword [cursor_column], COLUMN(50)
+  mov eax, 0
+  mov al, byte [esi+64]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
 
- PRINT '[a] codec 0 [b] codec 1', commands_str, LINE(9), COLUMN(1)
+  mov dword [cursor_column], COLUMN(62)
+  mov eax, 0
+  mov al, byte [esi+65]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
+
+  mov dword [cursor_column], COLUMN(73)
+  mov eax, 0
+  mov al, byte [esi+64]
+  mov dword [verb_node], eax
+  mov dword [verb_verb], 0xF02
+  mov dword [verb_command], 0x0
+  call hda_send_verb
+  mov eax, dword [hda_response]
+  mov dword [hex_print_value], eax
+  push esi
+  call print_hex
+  pop esi
+
+ add dword [cursor_line], LINESZ
+ add esi, 2
+ pop ecx
+ dec ecx
+ cmp ecx, 0
+ jne .print_values
 
  call redraw_screen
 
  .hda_halt:
-  call wait_for_keyboard
+  call wait_for_usb_keyboard
 
   cmp byte [key_code], KEY_ESC
   je developer_zone
-
-  cmp byte [key_code], KEY_A
-  je .key_a
-
-  cmp byte [key_code], KEY_B
-  je .key_b
  jmp .hda_halt
 
- .key_a:
-  mov dword [verb_codec], 0
-  call codec_find_widgets
- jmp dp_hda
+ .no_hda:
+ PRINT 'This sound card is not HDA audio compatibile', no_hda_str, LINE(1), COLUMN(1)
+ call redraw_screen
+ .no_hda_halt:
+  call wait_for_usb_keyboard
 
- .key_b:
-  mov dword [verb_codec], 1
-  call codec_find_widgets
- jmp dp_hda
+  cmp byte [key_code], KEY_ESC
+  je developer_zone
+ jmp .no_hda_halt
