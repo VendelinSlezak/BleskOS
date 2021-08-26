20bb4e838862b83cab160ee78e403ce67e753fc7
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-08-26 16:52:28 +0200

    Update board_developer_zone.asm

--- source/gui/board_developer_zone.asm
+++ source/gui/board_developer_zone.asm
@@ -9,11 +9,10 @@ developer_zone:
  ;text
  PRINT 'Developer zone', dz_up_str, LINE(1), COLUMN(1)
  PRINT '[a] Hardware info', dz_hw_str, LINE(3), COLUMN(1)
- PRINT '[b] VESA', dz_vesa_str, LINE(5), COLUMN(1)
- PRINT '[c] Intel HD Audio', dz_hda_str, LINE(7), COLUMN(1)
- PRINT '[d] IDE', dz_ide_str, LINE(9), COLUMN(1)
- PRINT '[e] Mouse', dz_mouse_str, LINE(11), COLUMN(1)
- PRINT '[f] Acpi', dz_acpi_str, LINE(13), COLUMN(1)
+ PRINT '[b] Intel HD Audio', dz_hda_str, LINE(5), COLUMN(1)
+ PRINT '[c] IDE', dz_ide_str, LINE(7), COLUMN(1)
+ PRINT '[d] Mouse', dz_mouse_str, LINE(9), COLUMN(1)
+ PRINT '[e] Acpi', dz_acpi_str, LINE(11), COLUMN(1)
 
  call redraw_screen
 
@@ -27,17 +26,14 @@ developer_zone:
   je hardware_info
 
   cmp byte [key_code], KEY_B
-  je dp_vesa
-
-  cmp byte [key_code], KEY_C
   je dp_hda
 
-  cmp byte [key_code], KEY_D
+  cmp byte [key_code], KEY_C
   je dp_ide
 
-  cmp byte [key_code], KEY_E
+  cmp byte [key_code], KEY_D
   je dp_mouse
 
-  cmp byte [key_code], KEY_F
+  cmp byte [key_code], KEY_E
   je dp_acpi
  jmp .developer_zone_halt
