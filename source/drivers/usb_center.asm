8d642182ee42b95b966ce50562247169a2493cbc
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-09-20 19:53:23 +0200

    Update usb_center.asm

--- source/drivers/usb_center.asm
+++ source/drivers/usb_center.asm
@@ -5,10 +5,10 @@
 %define EHCI 3
 %define XHCI 4
 
-ohci_num_of_ports dd 0
-uhci_num_of_ports dd 0
-ehci_num_of_ports dd 0
-xhci_num_of_ports dd 0
+ohci_num_of_controllers dd 0
+uhci_num_of_controllers dd 0
+ehci_num_of_controllers dd 0
+xhci_num_of_controllers dd 0
 
 ohci_controllers_base times 10 dd 0
 ohci_pointer dd ohci_controllers_base
@@ -19,12 +19,37 @@ uhci_pointer dd uhci_controllers_base
 ehci_controllers_base times 10 dd 0
 ehci_pointer dd ehci_controllers_base
 
+xhci_controllers_base times 10 dd 0
+xhci_pointer dd xhci_controllers_base
+
 usb_conf_number db 0
 usb_descriptor times 100 db 0
+usb_port_change dd 0
 
-init_usb_ports:
+init_usb_controllers:
+ mov dword [usb_port_change], 0
+ 
+ mov esi, xhci_controllers_base
+ mov ecx, dword [xhci_num_of_controllers]
+ cmp ecx, 0
+ je .skip_xhci
+ .init_xhci:
+ push ecx
+  mov eax, dword [esi]
+  mov dword [xhci_base], eax
+  push esi
+  call init_xhci
+  ;;WAIT 100
+  ;;mov dword [xhci_reset_every_device], 1
+  ;;call xhci_detect_devices
+  pop esi
+  add esi, 4
+ pop ecx
+ loop .init_xhci
+ .skip_xhci:
+ 
  mov esi, ehci_controllers_base
- mov ecx, dword [ehci_num_of_ports]
+ mov ecx, dword [ehci_num_of_controllers]
  cmp ecx, 0
  je .skip_ehci
  .init_ehci:
@@ -43,7 +68,7 @@ init_usb_ports:
  .skip_ehci:
 
  mov esi, ohci_controllers_base
- mov ecx, dword [ohci_num_of_ports]
+ mov ecx, dword [ohci_num_of_controllers]
  cmp ecx, 0
  je .skip_ohci
  .init_ohci:
@@ -63,7 +88,7 @@ init_usb_ports:
 
  mov dword [uhci_controller_number], 0
  mov esi, uhci_controllers_base
- mov ecx, dword [uhci_num_of_ports]
+ mov ecx, dword [uhci_num_of_controllers]
  cmp ecx, 0
  je .skip_uhci
  .init_uhci:
@@ -86,7 +111,7 @@ init_usb_ports:
 
 detect_usb_devices:
  mov esi, ehci_controllers_base
- mov ecx, dword [ehci_num_of_ports]
+ mov ecx, dword [ehci_num_of_controllers]
  cmp ecx, 0
  je .skip_ehci
  .detect_ehci:
@@ -103,7 +128,7 @@ detect_usb_devices:
  .skip_ehci:
 
  mov esi, ohci_controllers_base
- mov ecx, dword [ohci_num_of_ports]
+ mov ecx, dword [ohci_num_of_controllers]
  cmp ecx, 0
  je .skip_ohci
  .init_ohci:
@@ -121,7 +146,7 @@ detect_usb_devices:
 
  mov dword [uhci_controller_number], 0
  mov esi, uhci_controllers_base
- mov ecx, dword [uhci_num_of_ports]
+ mov ecx, dword [uhci_num_of_controllers]
  cmp ecx, 0
  je .skip_uhci
  .init_uhci:
@@ -138,6 +163,60 @@ detect_usb_devices:
  loop .init_uhci
  .skip_uhci:
 
+ ret
+ 
+usb_detect_port_change:
+ mov dword [usb_port_change], 0
+ 
+ mov esi, ehci_controllers_base
+ mov ecx, dword [ehci_num_of_controllers]
+ cmp ecx, 0
+ je .skip_ehci
+ .ehci_detect_port_change:
+ push ecx
+  mov eax, dword [esi]
+  mov dword [ehci_base], eax
+  call ehci_detect_port_change
+  add esi, 4
+ pop ecx
+ loop .ehci_detect_port_change
+ .skip_ehci:
+ 
+ cmp dword [usb_port_change], 1
+ je .done
+ 
+ mov esi, ohci_controllers_base
+ mov ecx, dword [ohci_num_of_controllers]
+ cmp ecx, 0
+ je .skip_ohci
+ .ohci_detect_port_change:
+ push ecx
+  mov eax, dword [esi]
+  mov dword [ohci_base], eax
+  call ohci_detect_port_change
+  add esi, 4
+ pop ecx
+ loop .ohci_detect_port_change
+ .skip_ohci:
+ 
+ cmp dword [usb_port_change], 1
+ je .done
+ 
+ mov esi, uhci_controllers_base
+ mov ecx, dword [uhci_num_of_controllers]
+ cmp ecx, 0
+ je .skip_uhci
+ .uhci_detect_port_change:
+ push ecx
+  mov ax, word [esi]
+  mov word [uhci_base], ax
+  call uhci_detect_port_change
+  add esi, 2
+ pop ecx
+ loop .uhci_detect_port_change
+ .skip_uhci:
+ 
+ .done:
  ret
 
 parse_usb_descriptor:
