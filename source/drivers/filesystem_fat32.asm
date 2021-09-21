732e8c270889b1b23b12db7d0c0b4c1dc58ac29e
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-09-21 20:52:50 +0200

    Update filesystem_fat32.asm

--- source/drivers/filesystem_fat32.asm
+++ source/drivers/filesystem_fat32.asm
@@ -46,6 +46,8 @@ fat_file_length dd 0
 fat_file_clusters dd 0
 fat_first_file_cluster dd 0
 
+fat_lfn_file_name times 52 db 0
+
 msd_read_mbr:
  mov esi, mbr
  mov ecx, 512
@@ -345,9 +347,14 @@ fat_write_file:
   pop esi
   cmp dword [msd_status], MSD_ERROR
   je .done
+
+  dec dword [fat_file_clusters]
+  cmp dword [fat_file_clusters], 0
+  je .write_clusters
  .skip_first_cluster:
 
  mov dword [fat_entry], 2
+ mov eax, dword [fat_file_clusters]
  ;ecx was calculated above
  .find_free_clusters:
   push eax
@@ -407,61 +414,106 @@ fat_write_file:
   add esi, 4
  loop .update_fat_table
 
+ mov esi, MEMORY_FILE_DESCRIPTOR
+ mov eax, dword [esi]
+ mov dword [fat_first_file_cluster], eax
+
  .done:
  ret
-
+ 
 convert_fat_folder_to_jus_folder:
  mov edi, MEMORY_FOLDER
- mov ecx, 10000
- .clear:
-  mov dword [edi], 0
-  add edi, 4
- loop .clear
-
+ mov eax, 0
+ mov ecx, 0x10000
+ rep stosb
+ 
  mov esi, MEMORY_FAT32_FOLDER
  mov edi, MEMORY_FOLDER
- mov ebp, 0
+ mov eax, fat_lfn_file_name 
  mov ecx, 2048 ;convert max 2048 items
  .convert_item:
   cmp byte [esi+11], 0
-  je .done
-  mov al, byte [esi+11]
-  and al, 0x2
-  cmp al, 0x2
-  je .next_item
-  mov al, byte [esi+11]
-  and al, 0x4
-  cmp al, 0x4
-  je .next_item
-  mov al, byte [esi+11]
-  and al, 0x8
-  cmp al, 0x8
-  je .next_item
+  je .done ;we are on last item
   cmp byte [esi+11], 0xF
+  je .lfn_entry
+  mov bl, byte [esi+11]
+  test bl, 0xE ;hidden, system, volume id
+  jnz .next_item
+  cmp byte [esi], 0xE5 ;not used item
   je .next_item
-  cmp byte [esi], 0xE5
-  je .next_item
-
+  
   ;name
-  mov eax, dword [esi]
-  mov dword [edi+16], eax
-  mov eax, dword [esi+4]
-  mov dword [edi+20], eax
-  mov byte [edi+25], 0
+  push esi
+  push edi
+  push ecx
+  
+  mov eax, fat_lfn_file_name
+  cmp word [eax], 0
+  je .short_name
+  
+  add edi, 16
+  mov eax, edi
+  push eax
+  
+  mov esi, fat_lfn_file_name
+  mov ecx, 50
+  rep movsw ;copy name
+  
+  pop eax
+  mov ecx, 50
+  .find_comma:
+   cmp word [eax], '.'
+   jne .next_loop
+   
+   mov word [eax], 0
+   jmp .comma_founded
+  .next_loop:
+  add eax, 2
+  loop .find_comma
+  .comma_founded:
+  
+  mov edi, fat_lfn_file_name
+  mov eax, 0
+  mov ecx, 52
+  rep stosw ;clear file name
+  jmp .long_name_end
+  
+  .short_name:
+  mov bx, 0
+  mov bl, byte [esi+0]
+  mov word [edi+16], bx
+  mov bl, byte [esi+1]
+  mov word [edi+18], bx
+  mov bl, byte [esi+2]
+  mov word [edi+20], bx
+  mov bl, byte [esi+3]
+  mov word [edi+22], bx
+  mov bl, byte [esi+4]
+  mov word [edi+24], bx
+  mov bl, byte [esi+5]
+  mov word [edi+26], bx
+  mov bl, byte [esi+6]
+  mov word [edi+28], bx
+  mov bl, byte [esi+7]
+  mov word [edi+30], bx
+  mov word [edi+32], 0
 
+  .long_name_end:
+  pop ecx
+  pop edi
+  pop esi
+  
   ;type
-  mov eax, dword [esi+8]
-  and eax, 0x00FFFFFF
-  mov dword [edi+56], eax
-  mov word [edi+14], 2
-
-  mov al, byte [esi+11]
-  and al, 0x10
-  cmp al, 0x10
-  jne .if_directory
-   mov word [edi+14], 1
-  .if_directory:
-
+  mov word [edi+14], 1 ;folder
+  test byte [esi+11], 0x10
+  jnz .if_not_directory
+   mov word [edi+14], 2 ;normal file
+   
+   mov ebx, dword [esi+8]
+   and ebx, 0x00FFFFFF
+   mov dword [edi+120], ebx
+  .if_not_directory:
+  
   ;year
   mov ax, word [esi+16]
   shr ax, 9
@@ -489,32 +541,68 @@ convert_fat_folder_to_jus_folder:
   shr ax, 5
   and ax, 0x3F
   mov byte [edi+13], al
-
+  
+  ;cluster
+  mov ax, word [esi+20]
+  shl eax, 16
+  mov ax, word [esi+26]
+  mov dword [edi+0], eax
+  
   ;size
   mov eax, dword [esi+28]
   mov ebx, 1024
   mov edx, 0
   div ebx ;convert to KB
   mov dword [edi+4], eax
-
-  ;cluster
-  mov ax, word [esi+20]
-  shl ax, 16
-  mov ax, word [esi+26]
-  mov dword [edi+60], eax
-
-  ;item offset
-  mov eax, ebp
-  mov dword [edi+50], eax
-
-  add edi, 64
+  
+  mov eax, fat_lfn_file_name
+  add edi, 128 ;next jus item
+  
+  jmp .next_item
+  
+  .lfn_entry:
+  mov ebx, 0
+  mov bl, byte [esi+0]
+  and bl, 0xF
+  cmp bl, 4
+  jg .next_item
+  mov eax, 26
+  dec ebx
+  mul ebx
+  add eax, fat_lfn_file_name
+  mov bx, word [esi+1]
+  mov word [eax+0], bx
+  mov bx, word [esi+3]
+  mov word [eax+2], bx
+  mov bx, word [esi+5]
+  mov word [eax+4], bx
+  mov bx, word [esi+7]
+  mov word [eax+6], bx
+  mov bx, word [esi+9]
+  mov word [eax+8], bx
+  mov bx, word [esi+14]
+  mov word [eax+10], bx
+  mov bx, word [esi+16]
+  mov word [eax+12], bx
+  mov bx, word [esi+18]
+  mov word [eax+14], bx
+  mov bx, word [esi+20]
+  mov word [eax+16], bx
+  mov bx, word [esi+22]
+  mov word [eax+18], bx
+  mov bx, word [esi+24]
+  mov word [eax+20], bx
+  mov bx, word [esi+28]
+  mov word [eax+22], bx
+  mov bx, word [esi+30]
+  mov word [eax+24], bx
+  
  .next_item:
- inc ebp
- add esi, 32
+ add esi, 32 ;next FAT32 item
  dec ecx
  cmp ecx, 0
  jne .convert_item
-
+ 
  .done:
  ret
 
@@ -525,31 +613,119 @@ convert_jus_folder_to_fat_folder:
   cmp dword [edi], 0
   je .done
 
-  mov eax, dword [edi+50]
+  mov eax, dword [edi+116]
   mov ebx, 32
   mul ebx
   add eax, MEMORY_FAT32_FOLDER ;pointer to right item
   mov esi, eax
 
   ;name
-  mov eax, dword [edi+16]
-  mov dword [esi], eax
-  mov eax, dword [edi+20]
-  mov dword [esi+4], eax
+  mov al, byte [edi+16]
+  mov byte [esi], al
+  mov al, byte [edi+18]
+  mov byte [esi+1], al
+  mov al, byte [edi+20]
+  mov byte [esi+2], al
+  mov al, byte [edi+22]
+  mov byte [esi+3], al
+  mov al, byte [edi+24]
+  mov byte [esi+4], al
+  mov al, byte [edi+26]
+  mov byte [esi+5], al
+  mov al, byte [edi+28]
+  mov byte [esi+6], al
+  mov al, byte [edi+30]
+  mov byte [esi+7], al
 
   ;type
-  mov al, byte [edi+56]
+  mov al, byte [edi+120]
   mov byte [esi+8], al
-  mov al, byte [edi+57]
+  mov al, byte [edi+121]
   mov byte [esi+9], al
-  mov al, byte [edi+58]
+  mov al, byte [edi+122]
   mov byte [esi+10], al
 
-  add edi, 64
+  ;size
+  mov eax, dword [edi+4]
+  mov ebx, 1024
+  mul ebx
+  mov dword [esi+28], eax
+
+  add edi, 128
  .next_item:
  dec ecx
  cmp ecx, 0
  jne .convert_item
 
  .done:
+ ret
+
+create_new_fat_entry:
+ mov esi, MEMORY_FAT32_FOLDER
+ mov ecx, 2048
+ .find_free_entry:
+  cmp byte [esi], 0
+  je .create_entry
+  add esi, 32
+ loop .find_free_entry
+ ret
+
+ .create_entry:
+ ;name
+ mov al, byte [edi+16]
+ mov byte [esi], al
+ mov al, byte [edi+18]
+ mov byte [esi+1], al
+ mov al, byte [edi+20]
+ mov byte [esi+2], al
+ mov al, byte [edi+22]
+ mov byte [esi+3], al
+ mov al, byte [edi+24]
+ mov byte [esi+4], al
+ mov al, byte [edi+26]
+ mov byte [esi+5], al
+ mov al, byte [edi+28]
+ mov byte [esi+6], al
+ mov al, byte [edi+30]
+ mov byte [esi+7], al
+
+ ;type
+ mov al, byte [edi+120]
+ mov byte [esi+8], al
+ mov al, byte [edi+121]
+ mov byte [esi+9], al
+ mov al, byte [edi+122]
+ mov byte [esi+10], al
+
+ ;attribute
+ mov al, 0x10
+ cmp word [edi+14], 1
+ je .write_attribute
+ mov al, 0x01
+ .write_attribute:
+ mov byte [esi+11], al
+
+ ;time
+ mov byte [esi+12], 0
+ mov byte [esi+13], 0
+ mov word [esi+14], 0x0821
+ mov word [esi+16], 0x0F01
+ mov word [esi+18], 0x0821
+ mov word [esi+22], 0x0821
+ mov word [esi+24], 0x0821
+
+ ;cluster
+ mov eax, dword [edi]
+ shr eax, 16
+ mov word [esi+20], ax
+ mov eax, dword [esi]
+ mov word [esi+26], ax
+
+ ;size
+ mov eax, dword [edi+4]
+ mov ebx, 1024
+ mul ebx
+ mov dword [esi+28], eax
+
+ call convert_fat_folder_to_jus_folder
  ret
