3dfa343bf33c666b541c006616e9ad02c7b5f936
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-10-04 19:33:53 +0200

    Update network_center.asm

--- source/drivers/network_center.asm
+++ source/drivers/network_center.asm
@@ -1,3 +1,43 @@
 ;BleskOS
 
 ethernet_card_id dd 0
+ethernet_card_irq_num db 0
+ethernet_card_io_base dw 0
+ethernet_card_mmio_base dd 0
+ethernet_card_mac times 6 db 0
+
+packet_pointer dd 0
+packet_length dd 0
+
+init_ethernet_card:
+ cmp dword [ethernet_card_id], 0
+ je .done
+ 
+ cmp word [ethernet_card_id], 0x1022
+ je init_nic_amd
+ 
+ .done:
+ ret
+ 
+nic_send_packet:
+ cmp word [ethernet_card_id], 0x1022
+ je nic_amd_send_packet
+ 
+ ret
+
+ethernet_card_irq:
+ cmp word [ethernet_card_id], 0x1022
+ jne .if_amd_nic
+  call nic_amd_irq
+  jmp .done
+ .if_amd_nic:
+ 
+ .done:
+ cmp byte [ethernet_card_irq_num], 7
+ jg .slave
+ EOI_MASTER_PIC
+ iret
+ 
+ .slave:
+ EOI_SLAVE_PIC
+ iret
