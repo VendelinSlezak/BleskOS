9895bf951db911db1d16f2636332c76a754cc2d6
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-10-21 18:01:23 +0200

    Update board_main.asm

--- source/gui/board_main.asm
+++ source/gui/board_main.asm
@@ -21,13 +21,17 @@ main_window:
  ;text
  PRINT 'Welcome in BleskOS', main_up_str, LINE(2), COLUMN(2)
  PRINT 'You can open programs with keyboard:', main_second_str, LINE(4), COLUMN(2)
- PRINT '[F1] Hardware centrum', main_hardware_centrum_str, LINE(6), COLUMN(2)
- PRINT '[F2] Developer zone', main_te_str, LINE(8), COLUMN(2)
+ PRINT '[a] Text editor', main_text_editor_str, LINE(6), COLUMN(2)
+ PRINT '[F1] Hardware centrum', main_hardware_centrum_str, LINE(8), COLUMN(2)
+ PRINT '[F2] Developer zone', main_te_str, LINE(10), COLUMN(2)
 
  call redraw_screen
 
  .main_gui_halt:
   call wait_for_usb_keyboard
+  
+  cmp byte [key_code], KEY_A
+  je text_editor
 
   cmp byte [key_code], KEY_F1
   je hardware_centrum
