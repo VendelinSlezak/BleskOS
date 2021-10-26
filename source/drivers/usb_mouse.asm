b0777952f70c316ef36371c26d417a2293d0a602
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-10-26 20:40:19 +0200

    Update usb_mouse.asm

--- source/drivers/usb_mouse.asm
+++ source/drivers/usb_mouse.asm
@@ -1,5 +1,21 @@
 ;BleskOS
 
+%macro TEST_CLICK_ZONE 5
+ cmp dword [ge_mouse_line], %2
+ jb .%1_not_pass
+ cmp dword [ge_mouse_line], %3
+ ja .%1_not_pass
+ cmp dword [ge_mouse_column], %4
+ jb .%1_not_pass
+ cmp dword [ge_mouse_column], %5
+ ja .%1_not_pass
+ mov eax, 1
+ jmp .%1_over
+ .%1_not_pass:
+ mov eax, 0
+ .%1_over:
+%endmacro
+
 usb_mouse_controller dd 0
 usb_mouse_base dd 0
 usb_mouse_controller_number dd 0
@@ -7,8 +23,11 @@ usb_mouse_address db 0
 usb_mouse_speed dd 0
 usb_mouse_endpoint dd 0
 usb_mouse_toggle dd 0
+usb_mouse_interval dd 0
 
 usb_mouse_data dd 0, 0
+old_usb_mouse_data dd 0
+usb_mouse_dnd dd 0 ;drag and drop
 
 usb_mouse_ohci_remove:
  mov ax, word [usb_mouse_base]
@@ -45,6 +64,7 @@ usb_mouse_uhci_remove:
  ret
 
 read_usb_mouse:
+ mov dword [usb_mouse_data], 0
  cmp dword [usb_mouse_controller], UHCI
  je .uhci
  cmp dword [usb_mouse_controller], OHCI
@@ -62,25 +82,20 @@ read_usb_mouse:
  mov dword [uhci_endpoint], eax
  mov al, byte [usb_mouse_address]
  mov byte [uhci_address], al
-
  mov eax, dword [usb_mouse_toggle]
  mov dword [uhci_toggle], eax
+ 
  call uhci_read_hid
+
  or dword [usb_mouse_toggle], 0xFFFFFFFE
  not dword [usb_mouse_toggle] ;reverse first bit
+ 
+ mov ebx, dword [MEMORY_UHCI+0x10200]
+ mov dword [usb_mouse_data], ebx
+ mov ebx, dword [MEMORY_UHCI+0x10204]
+ mov dword [usb_mouse_data+4], ebx
 
- mov eax, dword [MEMORY_UHCI+0x10200]
- mov dword [usb_mouse_data], eax
- mov eax, dword [MEMORY_UHCI+0x10204]
- mov dword [usb_mouse_data+4], eax
- cmp al, 3
- je .usb_mouse_3_bytes
- cmp al, 4
- je .usb_mouse_4_bytes
- cmp al, 7
- je .usb_mouse_7_bytes
-
- ret
+ jmp .parse_data
 
  .ohci:
  mov ax, word [usb_mouse_base]
@@ -91,26 +106,29 @@ read_usb_mouse:
  mov dword [ohci_endpoint], eax
  mov al, byte [usb_mouse_address]
  mov byte [ohci_address], al
-
  mov eax, dword [usb_mouse_toggle]
  mov dword [ohci_toggle], eax
+ 
  call ohci_read_hid
+ mov ebx, dword [MEMORY_OHCI+0x300]
+ mov dword [usb_mouse_data], ebx
+ mov ebx, dword [MEMORY_OHCI+0x304]
+ mov dword [usb_mouse_data+4], ebx
+ 
  or dword [usb_mouse_toggle], 0xFFFFFFFE
  not dword [usb_mouse_toggle] ;reverse first bit
-
- mov eax, dword [MEMORY_OHCI+0x300]
- mov dword [usb_mouse_data], eax
- mov eax, dword [MEMORY_OHCI+0x304]
- mov dword [usb_mouse_data+4], eax
+ 
+ jmp .parse_data
+ 
+ .parse_data:
  cmp al, 3
  je .usb_mouse_3_bytes
  cmp al, 4
  je .usb_mouse_4_bytes
  cmp al, 7
  je .usb_mouse_7_bytes
-
  ret
-
+ 
  .usb_mouse_3_bytes:
  and dword [usb_mouse_data], 0x00FFFFFF
  ret
@@ -128,13 +146,16 @@ read_usb_mouse:
  ret
 
 wait_for_usb_mouse:
+ mov eax, dword [usb_mouse_data]
+ mov dword [old_usb_mouse_data], eax
+ 
+ mov dword [usb_mouse_data], 0
  mov dword [ps2_mouse_wait], 1 ;PS/2 mouse
  mov dword [ps2_mouse_data_pointer], ps2_mouse_data
  mov dword [keyboard_wait], 1 ;PS/2 keyboard
  mov byte [key_code], 0
  mov word [key_unicode], 0
  mov dword [usb_keyboard_wait], 0
- mov dword [ticks], 0
 
  .wait:
   hlt
@@ -144,28 +165,36 @@ wait_for_usb_mouse:
 
   cmp dword [ps2_mouse_wait], 0
   je .ps2_mouse
-
-  cmp dword [ticks], 4
+  
+  mov eax, dword [ticks]
+  cmp eax, dword [usb_mouse_interval]
   jl .wait
-  mov dword [usb_mouse_data], 0
   call read_usb_mouse
-  cmp dword [usb_mouse_data], 0x0
-  jne .done
   mov dword [ticks], 0
-
-  inc dword [usb_keyboard_wait]
-  cmp dword [usb_keyboard_wait], 2
-  jl .wait
-  mov dword [usb_keyboard_data], 0
-  call read_usb_keyboard
-  cmp dword [key_code], 0x0
-  jne .done
-  mov dword [usb_keyboard_wait], 0
- jmp .wait
+  cmp dword [usb_mouse_data], 0
+  je .wait
+  
+  mov eax, dword [old_usb_mouse_data]
+  cmp dword [usb_mouse_data], eax
+  je .wait
+ jmp .test_drag_and_drop
 
  .ps2_mouse:
  mov eax, dword [ps2_mouse_data]
  mov dword [usb_mouse_data], eax
+ mov al, 0xFF
+ sub al, byte [usb_mouse_data+2]
+ mov byte [usb_mouse_data+2], al ;touchpad have reversed up/down byte, so we reverse it back
 
+ .test_drag_and_drop:
+ test byte [usb_mouse_data], 0x1
+ jz .no_dnd
+ cmp dword [usb_mouse_dnd], 2
+ je .done
+ inc dword [usb_mouse_dnd]
+ jmp .done
+ .no_dnd:
+ mov dword [usb_mouse_dnd], 0
+ 
  .done:
  ret
