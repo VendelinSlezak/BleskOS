4f6027fad9805140c6209209c7a7a622f8373d51
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-11-22 20:06:02 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 mkdir images
 
 nasm -f bin bootloader/bootloader.asm -o bootloader/bootloader.bin
-nasm -f bin source/bleskos.asm -o images/bleskos.bin -w-all
+nasm -f bin source/bleskos.asm -o images/bleskos.bin -Wfoo
 
 dd if=/dev/zero of=images/bleskos.hdd bs=1024 count=1440
 dd if=bootloader/bootloader.bin of=images/bleskos.hdd conv=notrunc seek=0
