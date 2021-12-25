172499e7569bdefbc1aebb07b4ce451df1f8849d
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-12-25 18:28:39 +0100

    Update sound_ac97.asm

--- source/drivers/sound_ac97.asm
+++ source/drivers/sound_ac97.asm
@@ -3,7 +3,9 @@
 ac97_nam_base dw 0
 ac97_nabm_base dw 0
 ac97_sound_data dd 0
+ac97_sample_rate dd 0
 ac97_volume dd 0
+ac97_last_entry dd 0
 
 init_ac97:
  cmp word [ac97_nam_base], 0
@@ -13,31 +15,52 @@ init_ac97:
 
  ;resume from cold reset
  BASE_OUTD ac97_nabm_base, 0x2C, 0x2
- WAIT 2
+ WAIT 20
  
  ;reset all streams
  BASE_OUTB ac97_nabm_base, 0x0B, 0x2
  BASE_OUTB ac97_nabm_base, 0x1B, 0x2
  BASE_OUTB ac97_nabm_base, 0x2B, 0x2
- WAIT 2
+ WAIT 20
 
  ;register reset
  BASE_OUTW ac97_nam_base, 0x0, 0xFF
 
  ;set volume
  BASE_OUTW ac97_nam_base, 0x2, 0x0
- BASE_OUTW ac97_nam_base, 0x18, 0x0808
+ BASE_OUTW ac97_nam_base, 0x18, 0x0
+ mov dword [ac97_volume], 100
 
  ;set output stream
  BASE_OUTD ac97_nabm_base, 0x10, MEMORY_AC97_BUFFER
- BASE_OUTB ac97_nabm_base, 0x15, 1
+ 
+ ;sample rate 48000
+ BASE_OUTW ac97_nam_base, 0x2A, 0x1
+ mov dword [ac97_sample_rate], 48000
+ call ac97_set_sample_rate
 
  .end:
  ret
+ 
+ac97_set_sample_rate:
+ mov eax, dword [ac97_sample_rate]
+
+ BASE_OUTW ac97_nam_base, 0x2C, ax
+ BASE_OUTW ac97_nam_base, 0x2E, ax
+ BASE_OUTW ac97_nam_base, 0x30, ax
+ BASE_OUTW ac97_nam_base, 0x32, ax
+ 
+ ret
 
 ac97_set_volume:
- ;calculate from 100 to AC97 volume range
- mov eax, dword [ac97_volume]
+ cmp dword [ac97_volume], 100
+ je .max_volume
+ cmp dword [ac97_volume], 0
+ je .no_volume
+
+ ;calculate to AC97 volume range
+ mov eax, 100
+ sub eax, dword [ac97_volume]
  mov ebx, 32
  mul ebx
  mov ebx, 100
@@ -48,18 +71,46 @@ ac97_set_volume:
  BASE_OUTW ac97_nam_base, 0x18, ax
 
  ret
+ 
+ .max_volume:
+ BASE_OUTW ac97_nam_base, 0x18, 0x0
+ ret
+ 
+ .no_volume:
+ BASE_OUTW ac97_nam_base, 0x18, 0x8000
+ ret
 
 ac97_play_sound:
- mov eax, dword [ac97_sound_data]
- mov dword [MEMORY_AC97_BUFFER+0], eax
- mov dword [MEMORY_AC97_BUFFER+4], 0x4000FFFE ;lenght 64 KB
-
+ mov eax, dword [ac97_last_entry]
+ mov ebx, 8
+ mul ebx
+ add eax, MEMORY_AC97_BUFFER
+ mov ebx, dword [ac97_sound_data]
+ mov dword [eax+0], ebx
+ mov dword [eax+4], 0x4000FFFE ;lenght 64 KB
+ 
  ;start transfer
- BASE_OUTB ac97_nabm_base, 0x1B, 0x2
+ mov eax, dword [ac97_last_entry]
+ BASE_OUTB ac97_nabm_base, 0x15, al
+ BASE_OUTB ac97_nabm_base, 0x1B, 0x1
+ inc dword [ac97_last_entry]
+ and dword [ac97_last_entry], 0x1F
 
+ ret
+ 
+ac97_sound_position:
+ BASE_INB ac97_nabm_base, 0x14
+ 
  ret
 
 ac97_stop_sound:
  BASE_OUTB ac97_nabm_base, 0x1B, 0x0
+ BASE_OUTW ac97_nabm_base, 0x16, 0x1C
+
+ ret
+ 
+ac97_resume_sound:
+ BASE_OUTB ac97_nabm_base, 0x1B, 0x1
+ BASE_OUTW ac97_nabm_base, 0x16, 0x1C
 
  ret
