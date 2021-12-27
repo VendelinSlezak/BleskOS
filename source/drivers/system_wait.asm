b973ca813eddf3a547586a63ff9adccdb98ad480
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-12-27 19:46:39 +0100

    Update system_wait.asm

--- source/drivers/system_wait.asm
+++ source/drivers/system_wait.asm
@@ -1,27 +1,35 @@
 ;BleskOS
 
 ticks dd 0
+ticks2 dd 0
+ticks3 dd 0
+enable_ticks3 dd 0
 al_save_state db 0
 
 %macro WAIT 1
  %if (%1 & 0x1)==0x0
- mov eax, %1/2+1
+ mov eax, (%1/2+1)
  %endif
  %if (%1 & 0x1)==0x1
- mov eax, %1/2+2
+ mov eax, (%1/2+2)
  %endif
  call wait_pit
 %endmacro
 
 set_pit:
  OUTB 0x43, 0x36
- OUTB 0x40, 2048 & 0xFF
- OUTB 0x40, 2048 >> 8
+ OUTB 0x40, 2386 & 0xFF ;500 Hz = every 2 miliseconds
+ OUTB 0x40, 2386 >> 8
 
  ret
 
 timer_irq:
  inc dword [ticks]
+ inc dword [ticks2]
+ cmp dword [enable_ticks3], 0
+ je .if_enable_ticks3
+  inc dword [ticks3]
+ .if_enable_ticks3:
  
  mov byte [al_save_state], al
  mov al, 0x20
