aaf1d675c4adb44d6b66631e699a9f65a9eb54f5
Klaykap <43180618+Klaykap@users.noreply.github.com>
2021-12-28 10:54:22 +0100

    Update media_viewer.asm

--- source/programs/media_viewer/media_viewer.asm
+++ source/programs/media_viewer/media_viewer.asm
@@ -5,21 +5,21 @@
 %define MV_SOUND 3
 
 %define MV_STOP 0
-%define MV_RESUME 1
-%define MV_PLAY 2
-%define MV_PLAY32 3
+%define MV_PLAY 1
 
 media_viewer_up_str db 'Media viewer', 0
 media_viewer_down_str db '[F2] Open file', 0
 media_viewer_down_close_str db '[F3] Close file', 0
 
 media_viewer_file_type dd 0
-media_viewer_file_mem dd 0
+media_viewer_file_size dd 0
+media_viewer_file_pointer dd 0
+media_viewer_file_data dd 0
+
 media_viewer_img_width dd 0
 media_viewer_img_heigth dd 0
 media_viewer_img_first_line dd 0
 media_viewer_img_first_column dd 0
-
 media_viewer_img_monitor_line dd 0
 media_viewer_img_monitor_column dd 0
 media_viewer_img_max_lines dd 0
@@ -36,12 +36,12 @@ media_viewer_sound_played_min dd 0
 media_viewer_sound_timer dd 0
 media_viewer_sound_state dd 0
 
-media_viewer_file_pointer dd 0
-
 media_viewer:
  DRAW_WINDOW media_viewer_up_str, media_viewer_down_str, 0x00FFEF, WHITE
- cmp dword [media_viewer_file_pointer], 0
- jne .draw_loaded_image
+ cmp dword [media_viewer_file_type], MV_IMAGE
+ je .draw_loaded_image
+ cmp dword [media_viewer_file_type], MV_SOUND
+ je media_viewer_play_file.redraw
  
  .redraw_screen:
  call redraw_screen
@@ -66,6 +66,11 @@ media_viewer:
   cmp dword [fd_return], FD_NO_FILE
   je media_viewer
   
+  mov eax, dword [allocated_memory_pointer]
+  mov dword [media_viewer_file_pointer], eax
+  mov eax, dword [allocated_size]
+  mov dword [media_viewer_file_size], eax
+  
   cmp dword [file_type], 'bmp'
   je .convert_bmp_file
   cmp dword [file_type], 'BMP'
@@ -77,12 +82,14 @@ media_viewer:
   jmp media_viewer
   
   .convert_bmp_file:
+  mov dword [media_viewer_file_type], MV_IMAGE
   call convert_bmp_file
   mov eax, dword [file_memory]
   mov dword [media_viewer_file_pointer], eax
   jmp media_viewer_draw_image_to_center
   
   .play_wav_file:
+  mov dword [media_viewer_file_type], MV_SOUND
   mov esi, dword [allocated_memory_pointer]
   jmp media_viewer_play_file
   
@@ -94,7 +101,7 @@ media_viewer_draw_image_to_center:
  je media_viewer ;invalid header
   
  add eax, 8 ;skip image info
- mov dword [media_viewer_file_mem], eax
+ mov dword [media_viewer_file_data], eax
  sub eax, 8
  mov bx, word [eax]
  mov word [media_viewer_img_width], bx
@@ -146,7 +153,7 @@ media_viewer_draw_image_to_center:
  jmp media_viewer_draw_image
 
 media_viewer_draw_image:
- mov eax, dword [media_viewer_file_mem]
+ mov eax, dword [media_viewer_file_data]
  mov dword [li_source_memory], eax
  mov eax, dword [media_viewer_img_width]
  mov dword [li_source_width], eax
@@ -167,33 +174,39 @@ media_viewer_draw_image:
  mov eax, dword [media_viewer_img_monitor_column]
  mov dword [li_destination_column], eax
  
- DRAW_WINDOW media_viewer_up_str, media_viewer_down_str, 0x00FFEF, WHITE
+ DRAW_WINDOW media_viewer_up_str, media_viewer_down_close_str, 0x00FFEF, WHITE
  call transfer_image
+ call redraw_screen
  
- jmp media_viewer.redraw_screen
+ .halt:
+  call wait_for_keyboard
+  
+  cmp byte [key_code], KEY_ESC
+  je main_window
+  
+  cmp byte [key_code], KEY_F3
+  je .close_file
+ jmp .halt
+ 
+ .close_file:
+  mov eax, dword [media_viewer_file_pointer]
+  mov dword [allocated_memory_pointer], eax
+  mov eax, dword [media_viewer_file_size]
+  mov dword [allocated_size], eax
+  call release_memory
+  mov dword [media_viewer_file_type], 0
+ jmp media_viewer
 
 media_viewer_play_file:
  cmp dword [ac97_nam_base], 0
  jne .ac97
- 
- ;Intel HD Audio
- mov eax, esi
- add eax, 40
- mov dword [hda_data_pointer], eax
- mov eax, dword [esi+4]
- mov dword [hda_data_length], eax
- mov word [hda_data_format], 0x0011
- call hda_play_sound
- 
- DRAW_WINDOW media_viewer_up_str, media_viewer_down_str, 0x00FFEF, WHITE
- call redraw_screen
- 
- WAIT 2000
- 
- HDA_OUTPUT_STREAM_POS
- PHEX eax
- 
- jmp media_viewer.redraw_screen
+ mov dword [media_viewer_file_type], 0
+ jmp media_viewer
+.redraw:
+ cmp dword [ac97_nam_base], 0
+ jne .ac97_redraw
+ mov dword [media_viewer_file_type], 0
+ jmp media_viewer
  
  .ac97:
  mov eax, dword [esi+4]
@@ -216,13 +229,21 @@ media_viewer_play_file:
  mov dword [media_viewer_sound_length], eax
  
  ;calculate number of seconds and minutes
- mov ebx, dword [esi+28] ;samples per second
+ mov ebx, dword [esi+28] ;bytes per second
  mov edx, 0
  div ebx
  mov dword [media_viewer_sound_length_sec], eax
+ 
+ mov eax, dword [esi+28] ;bytes per second
  mov ebx, 500
- mul ebx
+ mov edx, 0
+ div ebx
+ mov ebx, eax
+ mov eax, dword [media_viewer_sound_length]
+ mov edx, 0
+ div ebx
  mov dword [media_viewer_sound_length_milisec], eax
+ 
  mov eax, dword [media_viewer_sound_length_sec]
  mov ebx, 60
  mov edx, 0
@@ -266,15 +287,53 @@ media_viewer_play_file:
  SCREEN_Y_SUB eax, 20+LINESZ*2
  PRINT '[space] play [enter] stop [up/down] volume 1 step [right/left] volume 10 steps', playing_keys_str, eax, COLUMNSZ*1
  
+ ;played length of file
  SCREEN_Y_SUB eax, 20+LINESZ*2
  SCREEN_X_SUB ebx, COLUMNSZ*12
- PRINT '00:00/', time_str, eax, ebx
+ DRAW_SQUARE eax, ebx, COLUMNSZ*5, LINESZ, WHITE
+ mov eax, dword [ticks3]
+ mov ebx, 500
+ mov edx, 0
+ div ebx
+ mov ebx, 60
+ mov edx, 0
+ div ebx
+ mov dword [media_viewer_sound_played_sec], edx
+ mov dword [media_viewer_sound_played_min], eax
+ SCREEN_Y_SUB eax, 20+LINESZ*2
+ mov dword [cursor_line], eax
+ SCREEN_X_SUB eax, COLUMNSZ*12
+ mov dword [cursor_column], eax
+ mov dword [char_for_print], '0'
+ cmp dword [media_viewer_sound_played_min], 9
+ ja .if_played_min_above_10_redrawing
+  call print_char
+  add dword [cursor_column], COLUMNSZ
+ .if_played_min_above_10_redrawing:
+ mov eax, dword [media_viewer_sound_played_min]
+ mov dword [var_print_value], eax
+ call print_var
+ mov dword [char_for_print], ':'
+ call print_char
+ add dword [cursor_column], COLUMNSZ
+ mov dword [char_for_print], '0'
+ cmp dword [media_viewer_sound_played_sec], 9
+ ja .if_played_sec_above_10_redrawing
+  call print_char
+  add dword [cursor_column], COLUMNSZ
+ .if_played_sec_above_10_redrawing:
+ mov eax, dword [media_viewer_sound_played_sec]
+ mov dword [var_print_value], eax
+ call print_var
  
+ mov dword [char_for_print], '/'
+ call print_char
+ 
+ ;length of file
  SCREEN_Y_SUB eax, 20+LINESZ*2
  mov dword [cursor_line], eax
  SCREEN_X_SUB eax, COLUMNSZ*6
  mov dword [cursor_column], eax
- 
  mov dword [char_for_print], '0'
  cmp dword [media_viewer_sound_length_min], 9
  ja .if_min_above_10
@@ -284,11 +343,9 @@ media_viewer_play_file:
  mov eax, dword [media_viewer_sound_length_min]
  mov dword [var_print_value], eax
  call print_var
- 
  mov dword [char_for_print], ':'
  call print_char
  add dword [cursor_column], COLUMNSZ
- 
  mov dword [char_for_print], '0'
  cmp dword [media_viewer_sound_length_sec], 9
  ja .if_sec_above_10
@@ -299,15 +356,29 @@ media_viewer_play_file:
  mov dword [var_print_value], eax
  call print_var
  
+ ;volume
  SCREEN_Y_SUB ebx, 20+LINESZ*6
  PRINT 'Volume:', volume_str, ebx, COLUMNSZ*1
  SCREEN_Y_SUB ebx, 20+LINESZ*6
  mov eax, dword [ac97_volume]
  PRINT_VAR eax, ebx, COLUMNSZ*9
  
+ ;draw actual length of square
  SCREEN_Y_SUB eax, 20+LINESZ*4
  SCREEN_X_SUB ebx, COLUMNSZ*2
  DRAW_EMPTY_SQUARE eax, COLUMNSZ*1, ebx, LINESZ, BLACK
+ mov eax, dword [ticks3]
+ SCREEN_X_SUB ebx, COLUMNSZ*2+2
+ mul ebx
+ mov ebx, dword [media_viewer_sound_length_milisec]
+ mov edx, 0
+ div ebx
+ cmp eax, 0
+ je .skip_drawing_square_redrawing
+ SCREEN_Y_SUB ebx, 20+LINESZ*4-1
+ DRAW_SQUARE ebx, COLUMNSZ*1+1, eax, LINESZ-1, 0x0900FF ;deep blue
+ SCREEN_Y_SUB ebx, 20+LINESZ*4
+ .skip_drawing_square_redrawing:
  
  call redraw_screen
  
@@ -403,6 +474,9 @@ media_viewer_play_file:
    .if_stop_playing:
   .if_playing:
   
+  cmp byte [key_code], KEY_ESC
+  je .go_back
+  
   cmp byte [key_code], KEY_F3
   je .close_file
   
@@ -448,10 +522,21 @@ media_viewer_play_file:
   .if_all_is_played:
  jmp .halt
  
+ .go_back:
+  call ac97_stop_sound
+  mov dword [media_viewer_sound_state], MV_STOP
+  mov dword [enable_ticks3], 0
+ jmp main_window
+ 
  .close_file:
   call ac97_stop_sound
   call ac97_clear_buffer
+  mov eax, dword [media_viewer_file_pointer]
+  mov dword [allocated_memory_pointer], eax
+  mov eax, dword [media_viewer_file_size]
+  mov dword [allocated_size], eax
   call release_memory
+  mov dword [media_viewer_file_type], 0
  jmp media_viewer
  
  .volume_up:
