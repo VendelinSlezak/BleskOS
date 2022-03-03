7ddab797b4d529217a60377352c9ae66544c1534
Klaykap <43180618+Klaykap@users.noreply.github.com>
2022-03-03 18:09:52 +0100

    Update bleskos.asm

--- source/bleskos.asm
+++ source/bleskos.asm
@@ -78,37 +78,99 @@ start:
  %include "source/programs/internet_browser/internet_browser.asm"
  %include "source/programs/document_editor/document_editor.asm"
 
+%macro START_SCREEN_DRAW_SQUARE 1
+ mov eax, dword [screen_x_center]
+ sub eax, 167
+ mov ebx, dword [screen_y_center]
+ add ebx, 31
+ DRAW_SQUARE ebx, eax, %1, 14, 0x0900FF
+ mov ebx, dword [screen_y_center]
+ add ebx, 31
+ REDRAW_LINES_SCREEN ebx, 14
+%endmacro
+
+%macro START_SCREEN_PRINT 2
+ jmp %2_over
+ %2 db %1, 0
+ %2_over:
+ mov eax, dword [screen_y_center]
+ add eax, 65
+ mov ebx, dword [screen_x]
+ DRAW_SQUARE eax, 0, ebx, 10, 0x00C000
+ mov esi, %2
+ mov eax, dword [screen_y_center]
+ add eax, 65
+ call print_to_message_window
+ mov eax, dword [screen_y_center]
+ add eax, 65
+ REDRAW_LINES_SCREEN eax, 10
+%endmacro
+
+numberstr times 10 dw 0
+
 start_bleskos:
  call init_graphic
  CLEAR_SCREEN 0x00C000
+ mov eax, dword [screen_x_center]
+ sub eax, 224
+ mov ebx, dword [screen_y_center]
+ sub ebx, 76
+ mov dword [size_of_text], 8
+ mov dword [color], BLACK
+ PRINT 'BleskOS', start_screen_bleskos_str, ebx, eax
+ mov eax, dword [screen_x_center]
+ sub eax, 52
+ mov ebx, dword [screen_y_center]
+ mov dword [size_of_text], 1
+ PRINT 'Alpha version', start_screen_alpha_1_str, ebx, eax
+ mov eax, dword [screen_x_center]
+ sub eax, 168
+ mov ebx, dword [screen_y_center]
+ add ebx, 30
+ DRAW_EMPTY_SQUARE ebx, eax, 336, 15, BLACK
  call redraw_screen
 
- PSTR 'Reading size of RAM memory...', bootstr_ramsize
+ START_SCREEN_PRINT 'Reading size of RAM memory...', start_screen_ramsize
+ call redraw_screen
  call read_memory_size
- PSTR 'Enabling interrupts...', bootstr_interrupts
+ START_SCREEN_DRAW_SQUARE 30
+ START_SCREEN_PRINT 'Enabling interrupts...', bootstr_interrupts
  call init_idt
  call set_pit
- PSTR 'Scanning PCI...', bootstr_pci
+ START_SCREEN_DRAW_SQUARE 60
+ START_SCREEN_PRINT 'Scanning PCI...', bootstr_pci
  call scan_pci
  call init_keyboard
- PSTR 'Reading ACPI table...', bootstr_acpi
+ START_SCREEN_DRAW_SQUARE 90
+ START_SCREEN_PRINT 'Reading ACPI table...', bootstr_acpi
  call read_acpi
- PSTR 'Initalizing IDE drives...', bootstr_ide
+ START_SCREEN_DRAW_SQUARE 120
+ START_SCREEN_PRINT 'Initalizing IDE drives...', bootstr_ide
  call init_ahci
  call init_ide
- PSTR 'Initalizing sound card...', bootstr_sound_card
+ START_SCREEN_DRAW_SQUARE 150
+ START_SCREEN_PRINT 'Initalizing sound card...', bootstr_sound_card
  call init_ac97
  call init_sound_card
- PSTR 'Initalizing PS/2 devices...', bootstr_ps2
+ START_SCREEN_DRAW_SQUARE 180
+ START_SCREEN_PRINT 'Initalizing PS/2 devices...', bootstr_ps2
  call init_ps2_controller
  call enable_touchpad
- PSTR 'Read JUS informations...', bootstr_jus
+ START_SCREEN_DRAW_SQUARE 210
+ START_SCREEN_PRINT 'Read JUS informations...', bootstr_jus
  call init_jus
- PSTR 'Initalizing USB ports...', bootstr_usb
+ START_SCREEN_DRAW_SQUARE 240
+ START_SCREEN_PRINT 'Initalizing USB ports...', bootstr_usb
  call init_usb_controllers
- PSTR 'Allocating memory for programs...', bootstr_mem_alloc
+ START_SCREEN_DRAW_SQUARE 270
+ START_SCREEN_PRINT 'Initalizing network cards...', bootstr_nic
+ ;call init_ethernet_card
+ ;call init_network_stack
+ START_SCREEN_DRAW_SQUARE 300
+ START_SCREEN_PRINT 'Allocating memory for programs...', bootstr_mem_alloc
  call init_memory_of_programs
-
+ START_SCREEN_DRAW_SQUARE 335
+ 
  call main_window
 
  HALT
