c692779801ba6e78ca795b8021c16dc6a60a6b34
Klaykap <43180618+Klaykap@users.noreply.github.com>
2022-03-12 19:12:05 +0100

    Update text_editor.asm

--- source/programs/text_editor/text_editor.asm
+++ source/programs/text_editor/text_editor.asm
@@ -21,14 +21,27 @@ te_pointer_end dd 0
 te_cursor_offset dd 0
 te_length_of_text dd 0
 
+te_mouse_line dd 20
+te_mouse_column dd 0
+te_mouse_cursor_text_pointer dd 0
+te_mouse_cursor_first_pointer dd 0
+te_mouse_cursor_second_pointer dd 0
+
 text_editor_file_pointer dd 0
  
 text_editor: 
  call te_draw_text
  call redraw_screen
  
+ mov dword [mcursor_up_side], 20
+ mov dword [mcursor_left_side], 0
+ SCREEN_Y_SUB eax, 20
+ mov dword [mcursor_down_side], eax
+ mov eax, dword [screen_x]
+ mov dword [mcursor_right_side], eax
+ 
  .te_halt:
-  call wait_for_usb_keyboard
+  call wait_for_usb_mouse
   
   cmp byte [key_code], KEY_ESC
   je main_window
@@ -60,6 +73,13 @@ text_editor:
   cmp byte [key_code], KEY_DOWN
   je .key_down
   
+  cmp byte [keyboard_ctrl], 1
+  je .key_ctrl
+  
+  cmp byte [usb_mouse_data], 0
+  jne .mouse_event
+  
+  .test_if_add_char:
   cmp word [key_unicode], 0
   jne .draw_char
  jmp .te_halt
@@ -153,6 +173,10 @@ text_editor:
  jmp .te_redraw_screen
   
  .key_backspace:
+  mov eax, dword [te_mouse_cursor_first_pointer]
+  cmp eax, dword [te_mouse_cursor_second_pointer]
+  jne .delete_selected_text
+  
   cmp dword [te_cursor_offset], 0
   je .te_halt
   
@@ -165,6 +189,9 @@ text_editor:
   call te_move_first_line_up
   
   .backspace:
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+  
   sub dword [te_pointer_end], 2
   sub dword [te_pointer], 2
   dec dword [te_cursor_offset]
@@ -183,6 +210,10 @@ text_editor:
  jmp .te_redraw_screen
  
  .key_delete:
+  mov eax, dword [te_mouse_cursor_first_pointer]
+  cmp eax, dword [te_mouse_cursor_second_pointer]
+  jne .delete_selected_text
+  
   mov eax, dword [te_pointer_end]
   cmp dword [te_pointer], eax
   je .te_halt
@@ -200,8 +231,51 @@ text_editor:
   cld
   rep movsw ;move all text
  jmp .te_redraw_screen
+ .delete_selected_text:
+  cmp eax, dword [te_mouse_cursor_second_pointer]
+  jb .delete_if_second_below
+   mov ebx, dword [te_mouse_cursor_second_pointer]
+   mov dword [te_mouse_cursor_first_pointer], ebx
+   mov dword [te_mouse_cursor_second_pointer], eax
+  .delete_if_second_below:
+  
+  mov eax, dword [te_mouse_cursor_first_pointer]
+  mov dword [te_pointer], eax
+  sub eax, dword [text_editor_mem]
+  shr eax, 1 ;div 2
+  mov dword [te_cursor_offset], eax
+  
+  mov eax, dword [te_mouse_cursor_second_pointer]
+  sub eax, dword [te_mouse_cursor_first_pointer] ;length of selected text
+  sub dword [te_pointer_end], eax
+  shr eax, 1 ;div 2
+  sub dword [te_length_of_text], eax
+  shl eax, 1 ;mul 2
+  push eax
+  
+  mov edi, dword [te_mouse_cursor_first_pointer]
+  mov esi, dword [te_mouse_cursor_second_pointer]
+  mov ecx, dword [te_pointer_end]
+  add ecx, eax
+  sub ecx, dword [te_mouse_cursor_second_pointer]
+  add ecx, 2
+  cld
+  rep movsb ;move all text
+  
+  pop eax
+  mov edi, dword [te_pointer_end]
+  mov ecx, eax
+  mov eax, 0
+  rep stosb ;clear previous text
+  
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+ jmp .te_redraw_screen
  
  .key_left:
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+  
   cmp dword [te_cursor_offset], 0
   je .te_halt
   
@@ -228,6 +302,9 @@ text_editor:
  jmp .te_redraw_screen
  
  .key_right:
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+  
   mov eax, dword [te_pointer]
   cmp eax, dword [text_editor_end_mem]
   je .te_halt
@@ -262,6 +339,9 @@ text_editor:
  jmp .te_redraw_screen
  
  .key_up:
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+  
   cmp dword [te_cursor_offset], 0
   je .te_halt
   
@@ -285,6 +365,9 @@ text_editor:
  jmp .te_redraw_screen
 
  .key_down:
+  mov dword [te_mouse_cursor_first_pointer], 0
+  mov dword [te_mouse_cursor_second_pointer], 0
+  
   mov eax, dword [text_editor_end_mem]
   cmp dword [te_pointer], eax
   je .te_halt
@@ -312,7 +395,256 @@ text_editor:
   jmp .move_cursor_down
  jmp .te_redraw_screen
  
+ .key_ctrl:
+  cmp byte [key_code], KEY_C
+  jne .if_ctrl_copy
+   mov eax, dword [te_mouse_cursor_first_pointer]
+   cmp eax, dword [te_mouse_cursor_second_pointer]
+   je .te_halt ;nothing is selected
+   jb .key_ctrl_copy_if_change_pointer
+    mov ebx, dword [te_mouse_cursor_second_pointer]
+    mov dword [te_mouse_cursor_first_pointer], ebx
+    mov dword [te_mouse_cursor_second_pointer], eax
+   .key_ctrl_copy_if_change_pointer
+   
+   mov eax, dword [te_mouse_cursor_first_pointer]
+   mov dword [copy_source], eax
+   mov ebx, dword [te_mouse_cursor_second_pointer]
+   sub ebx, eax
+   mov dword [copy_length], ebx
+   call copy_to_memory
+   jmp .te_halt
+  .if_ctrl_copy:
+  
+  cmp byte [key_code], KEY_V
+  jne .if_ctrl_insert
+   ;erase what is selected
+   mov eax, dword [te_mouse_cursor_first_pointer]
+   cmp eax, dword [te_mouse_cursor_second_pointer]
+   je .ctrl_insert_if_selected_text
+    cmp eax, dword [te_mouse_cursor_second_pointer]
+    jb .ctrl_insert_if_second_below
+     mov ebx, dword [te_mouse_cursor_second_pointer]
+     mov dword [te_mouse_cursor_first_pointer], ebx
+     mov dword [te_mouse_cursor_second_pointer], eax
+    .ctrl_insert_if_second_below:
+  
+    mov eax, dword [te_mouse_cursor_first_pointer]
+    mov dword [te_pointer], eax
+    sub eax, dword [text_editor_mem]
+    shr eax, 1 ;div 2
+    mov dword [te_cursor_offset], eax
+  
+    mov eax, dword [te_mouse_cursor_second_pointer]
+    sub eax, dword [te_mouse_cursor_first_pointer] ;length of selected text
+    sub dword [te_pointer_end], eax
+    shr eax, 1 ;div 2
+    sub dword [te_length_of_text], eax
+    shl eax, 1 ;mul 2
+    push eax
+  
+    mov edi, dword [te_mouse_cursor_first_pointer]
+    mov esi, dword [te_mouse_cursor_second_pointer]
+    mov ecx, dword [te_pointer_end]
+    add ecx, eax
+    sub ecx, dword [te_mouse_cursor_second_pointer]
+    add ecx, 2
+    cld
+    rep movsb ;move all text
+  
+    pop eax
+    mov edi, dword [te_pointer_end]
+    mov ecx, eax
+    mov eax, 0
+    rep stosb ;clear previous text
+  
+    mov dword [te_mouse_cursor_first_pointer], 0
+    mov dword [te_mouse_cursor_second_pointer], 0
+   .ctrl_insert_if_selected_text:
+  
+   cmp dword [copy_length], 0
+   je .te_halt
+   
+   mov eax, dword [te_pointer]
+   cmp dword [eax], 0
+   je .ctrl_insert
+   mov edi, dword [te_pointer_end]
+   mov esi, dword [te_pointer_end]
+   sub esi, dword [copy_length]
+   mov ecx, dword [te_pointer_end]
+   sub ecx, dword [te_pointer]
+   std
+   rep movsb ;move all text
+   cld
+  
+   .ctrl_insert:
+   mov eax, dword [te_pointer]
+   mov dword [copy_destination], eax
+   call copy_from_memory
+   
+   mov eax, dword [copy_length]
+   add dword [te_pointer], eax
+   add dword [te_pointer_end], eax
+   shr eax, 1 ;div 2
+   add dword [te_cursor_offset], eax
+   add dword [te_length_of_text], eax
+   
+   jmp .te_redraw_screen
+  .if_ctrl_insert:
+ jmp .te_halt
+ 
+ .mouse_event:
+  mov eax, dword [te_mouse_line]
+  mov dword [cursor_line], eax
+  mov eax, dword [te_mouse_column]
+  mov dword [cursor_column], eax
+  call move_mouse_cursor
+  mov eax, dword [cursor_line]
+  mov dword [te_mouse_line], eax
+  mov eax, dword [cursor_column]
+  mov dword [te_mouse_column], eax
+  
+  cmp dword [usb_mouse_dnd], 0x1
+  je .mouse_click
+  cmp dword [usb_mouse_dnd], 0x2
+  je .mouse_drag_and_drop
+ jmp .test_if_add_char
+ 
+ .mouse_click:
+  mov eax, dword [te_pointer]
+  mov dword [te_mouse_cursor_text_pointer], eax
+  call te_draw_text
+  mov eax, dword [te_mouse_cursor_text_pointer]
+  cmp eax, dword [te_pointer]
+  je .mouse_double_click
+  mov dword [te_pointer], eax
+  mov dword [te_mouse_cursor_first_pointer], eax
+  mov dword [te_mouse_cursor_second_pointer], eax
+  mov dword [te_mouse_cursor_text_pointer], 0
+  sub eax, dword [text_editor_mem]
+  shr eax, 1 ;div 2
+  mov dword [te_cursor_offset], eax
+ jmp .te_redraw_screen
+ 
+ .mouse_double_click:
+  mov dword [te_mouse_cursor_text_pointer], 0
+  
+  mov eax, dword [te_pointer]
+  mov ebx, eax
+  mov ecx, eax
+  mov edx, eax
+  mov dword [te_mouse_cursor_first_pointer], eax
+  mov dword [te_mouse_cursor_second_pointer], eax
+  .mouse_double_click_go_left:
+   cmp eax, dword [text_editor_mem]
+   je .mouse_double_click_go_right
+   sub ebx, 2
+   cmp word [ebx], ' '
+   je .mouse_double_click_go_right
+   cmp word [ebx], 0xA
+   je .mouse_double_click_go_right
+   cmp word [ebx], '"'
+   je .mouse_double_click_go_right
+   cmp word [ebx], '('
+   je .mouse_double_click_go_right
+   cmp word [ebx], ')'
+   je .mouse_double_click_go_right
+   cmp word [ebx], '['
+   je .mouse_double_click_go_right
+   cmp word [ebx], ']'
+   je .mouse_double_click_go_right
+   cmp word [ebx], '<'
+   je .mouse_double_click_go_right
+   cmp word [ebx], '>'
+   je .mouse_double_click_go_right
+   sub eax, 2
+  jmp .mouse_double_click_go_left
+  
+  .mouse_double_click_go_right:
+   cmp word [ecx], 0
+   je .mouse_double_click_set_values
+   cmp word [ecx], ' '
+   je .mouse_double_click_set_values
+   cmp word [ecx], 0xA
+   je .mouse_double_click_set_values
+   cmp word [ecx], '"'
+   je .mouse_double_click_set_values
+   cmp word [ecx], '('
+   je .mouse_double_click_set_values
+   cmp word [ecx], ')'
+   je .mouse_double_click_set_values
+   cmp word [ecx], '['
+   je .mouse_double_click_set_values
+   cmp word [ecx], ']'
+   je .mouse_double_click_set_values
+   cmp word [ecx], '<'
+   je .mouse_double_click_set_values
+   cmp word [ecx], '>'
+   je .mouse_double_click_set_values
+   add ecx, 2
+  jmp .mouse_double_click_go_right
+  
+  .mouse_double_click_set_values:
+  mov dword [te_mouse_cursor_first_pointer], eax
+  mov dword [te_mouse_cursor_second_pointer], ecx
+ jmp .te_redraw_screen
+ 
+ .mouse_drag_and_drop:
+  mov eax, dword [te_pointer]
+  mov dword [te_mouse_cursor_text_pointer], eax
+  call te_draw_text
+  mov eax, dword [te_mouse_cursor_text_pointer]
+  cmp eax, dword [te_pointer]
+  je .mouse_double_click
+  mov dword [te_mouse_cursor_second_pointer], eax
+  mov dword [te_mouse_cursor_text_pointer], 0
+ jmp .te_redraw_screen
+ 
  .draw_char:
+  ;test if something is not selected
+  mov eax, dword [te_mouse_cursor_first_pointer]
+  cmp eax, dword [te_mouse_cursor_second_pointer]
+  je .draw_char_if_selected_text
+   cmp eax, dword [te_mouse_cursor_second_pointer]
+   jb .draw_char_if_second_below
+    mov ebx, dword [te_mouse_cursor_second_pointer]
+    mov dword [te_mouse_cursor_first_pointer], ebx
+    mov dword [te_mouse_cursor_second_pointer], eax
+   .draw_char_if_second_below:
+  
+   mov eax, dword [te_mouse_cursor_first_pointer]
+   mov dword [te_pointer], eax
+   sub eax, dword [text_editor_mem]
+   shr eax, 1 ;div 2
+   mov dword [te_cursor_offset], eax
+  
+   mov eax, dword [te_mouse_cursor_second_pointer]
+   sub eax, dword [te_mouse_cursor_first_pointer] ;length of selected text
+   sub dword [te_pointer_end], eax
+   shr eax, 1 ;div 2
+   sub dword [te_length_of_text], eax
+   shl eax, 1 ;mul 2
+   push eax
+  
+   mov edi, dword [te_mouse_cursor_first_pointer]
+   mov esi, dword [te_mouse_cursor_second_pointer]
+   mov ecx, dword [te_pointer_end]
+   add ecx, eax
+   sub ecx, dword [te_mouse_cursor_second_pointer]
+   add ecx, 2
+   cld
+   rep movsb ;move all text
+  
+   pop eax
+   mov edi, dword [te_pointer_end]
+   mov ecx, eax
+   mov eax, 0
+   rep stosb ;clear previous text
+  
+   mov dword [te_mouse_cursor_first_pointer], 0xFFFFFFFF
+   mov dword [te_mouse_cursor_second_pointer], 0xFFFFFFFF
+  .draw_char_if_selected_text:
+ 
   call text_editor_found_cursor_position
   mov eax, dword [te_max_column]
   dec eax
@@ -375,6 +707,10 @@ text_editor:
   mov ebx, LINESZ
   mul ebx
   REDRAW_LINES_SCREEN eax, LINESZ
+  
+  cmp dword [te_mouse_cursor_first_pointer], 0xFFFFFFFF
+  jne .te_halt
+  call redraw_screen
  jmp .te_halt
  
  .te_redraw_screen:
@@ -436,10 +772,6 @@ te_draw_text:
   cmp esi, dword [te_pointer]
   jne .if_draw_cursor
    dec dword [cursor_line]
-   cmp dword [te_draw_column], 0
-   je .if_not_first_column
-    ;dec dword [cursor_column]
-   .if_not_first_column:
    
    mov dword [column_heigth], LINESZ
    mov dword [color], 0x4E4E4E ;gray
@@ -447,25 +779,106 @@ te_draw_text:
    call draw_column
    pop esi
    
-   inc dword [cursor_line]
-   cmp dword [te_draw_column], 0
-   je .if_not_first_column_2
-    ;inc dword [cursor_column]
-   .if_not_first_column_2:
-   
+   inc dword [cursor_line]   
    mov dword [color], BLACK
   .if_draw_cursor:
   
-  ;get number of char
-  mov ax, word [esi]
+  ;test mouse cursor position
+  cmp dword [te_mouse_cursor_text_pointer], 0
+  je .if_mouse_cursor_here
+  mov eax, dword [te_mouse_line]
+  mov ebx, dword [cursor_line]
+  dec ebx
+  cmp eax, ebx
+  jb .if_mouse_cursor_here ;if cursor below first line of char
+  add ebx, 10 ;size of line
+  cmp eax, ebx
+  ja .if_mouse_cursor_here ;if cursor above last line of char
+  mov eax, dword [te_mouse_column]
+  mov ebx, dword [cursor_column]
+  cmp eax, ebx
+  jb .if_mouse_cursor_here ;if cursor above first column of char
+  cmp word [esi], 0xA
+  je .if_mouse_cursor_move_cursor_here ;if end of line, move cursor here
+   ;mouse cursor is on this char
+   add ebx, 4 ;half of char
+   cmp eax, ebx
+   jb .if_mouse_cursor_move_cursor_here ;cursor in first half of char - move cursor pointer to this char
+   jmp .if_mouse_cursor_move_cursor_next ;cursor in second half of char - move cursor pointer on next char
+  .if_mouse_cursor_move_cursor_here:
+   mov dword [te_mouse_cursor_text_pointer], esi
+   jmp .if_mouse_cursor_here
+  .if_mouse_cursor_move_cursor_next:
+   mov eax, esi
+   add eax, 2
+   mov dword [te_mouse_cursor_text_pointer], eax
+  .if_mouse_cursor_here:
   
-  cmp ax, 0xA ;enter
+  ;draw background of char
+  mov eax, dword [te_mouse_cursor_first_pointer]
+  mov ebx, dword [te_mouse_cursor_second_pointer]
+  cmp eax, ebx
+  je .if_draw_background
+  ja .second_is_above
+   cmp esi, eax
+   jb .if_draw_background
+   cmp ebx, esi
+   je .if_draw_background
+   jb .if_draw_background
+   
+   push dword [cursor_line]
+   push dword [cursor_column]
+  
+   dec dword [cursor_line]
+   mov dword [square_length], 8
+   mov dword [square_heigth], 10
+   mov dword [color], 0x00A0FF
+   cmp esi, dword [te_pointer]
+   jne .if_cursor_background_1
+    inc dword [cursor_column]
+    mov dword [square_length], 7
+   .if_cursor_background_1:
+   call draw_square
+   mov dword [color], BLACK
+   
+   pop dword [cursor_column]
+   pop dword [cursor_line]
+   jmp .if_draw_background
+  .second_is_above:
+   cmp esi, ebx
+   jb .if_draw_background
+   cmp eax, esi
+   je .if_draw_background
+   jb .if_draw_background
+   
+   push dword [cursor_line]
+   push dword [cursor_column]
+  
+   dec dword [cursor_line]
+   mov dword [square_length], 8
+   mov dword [square_heigth], 10
+   mov dword [color], 0x00A0FF
+   cmp esi, dword [te_pointer]
+   jne .if_cursor_background_2
+    inc dword [cursor_column]
+    mov dword [square_length], 7
+   .if_cursor_background_2:
+   call draw_square
+   mov dword [color], BLACK
+   
+   pop dword [cursor_column]
+   pop dword [cursor_line]
+  .if_draw_background:
+  
+  ;test end of line
+  cmp word [esi], 0xA ;enter
   je .enter
   
   ;draw char on screen
   cmp ebp, 1
   je .not_draw_char
   mov dword [char_for_print], 0
+  mov ax, word [esi]
   mov word [char_for_print], ax
   call print_char
   .not_draw_char:
@@ -516,10 +929,33 @@ te_draw_text:
   call draw_column
  .if_draw_cursor_2:
  
+ ;test mouse cursor position
+ cmp dword [te_mouse_cursor_text_pointer], 0
+ je .if_end_mouse_cursor_here
+ mov eax, dword [te_mouse_line]
+ inc eax
+ mov ebx, dword [cursor_line]
+ cmp eax, ebx
+ jb .if_end_mouse_cursor_here ;if cursor below first line of char
+ mov eax, dword [te_mouse_column]
+ mov ebx, dword [cursor_column]
+ cmp eax, ebx
+ jb .if_end_mouse_cursor_here ;if cursor above first column of char
+  mov eax, dword [te_pointer_end]
+  mov dword [te_mouse_cursor_text_pointer], eax
+ .if_end_mouse_cursor_here:
+ 
  .done:
  pop dword [te_draw_column]
  pop dword [te_draw_line]
  DRAW_WINDOW_BORDERS text_editor_up_str, text_editor_down_str, 0xFF0000
+ 
+ mov eax, dword [te_mouse_line]
+ mov dword [cursor_line], eax
+ mov eax, dword [te_mouse_column]
+ mov dword [cursor_column], eax
+ call read_cursor_bg
+ call draw_cursor
  
  ret
 
