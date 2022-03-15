9244aa2f6d9e8ba03228bbea5bd3825801afb16e
Klaykap <43180618+Klaykap@users.noreply.github.com>
2022-03-15 21:10:33 +0100

    Update graphic_editor.asm

--- source/programs/graphic_editor/graphic_editor.asm
+++ source/programs/graphic_editor/graphic_editor.asm
@@ -1,7 +1,7 @@
 ;BleskOS
 
 graphic_editor_up_str db 'Graphic editor', 0
-graphic_editor_down_str db '[F1] Save file [F2] Open file [F3] New file [q] Mouse on image [arrows] Move image', 0
+graphic_editor_down_str db '[F1] Save file [F2] Open file [F3] New file [F4/5] Step +/- [q] Mouse on image [arrows] Move image', 0
 ge_new_file_up_str db 'Please select size of new image:', 0
 ge_new_file_down_str db '[a] 100x100 [b] 640x480 [c] 800x600 [d] 1024x768', 0
 
@@ -10,7 +10,13 @@ ge_drawing_state dd 0
 %define GE_FREE_DRAW 0
 %define GE_LINE_DRAW 1
 %define GE_SQUARE 2
+%define GE_CIRCLE 3
+%define GE_FILL 4
 ge_draw_object dd GE_FREE_DRAW
+%define GE_EMPTY 0
+%define GE_FULL 1
+ge_tool_option dd 0
+
 ge_draw_click1_line dd 0
 ge_draw_click1_column dd 0
 ge_draw_click2_line dd 0
@@ -28,6 +34,9 @@ ge_image_first_show_line dd 0
 ge_image_first_show_column dd 0
 ge_image_bytes_per_line dd 640*4
 ge_image_pointer dd 0
+ge_image_step_back_pointer dd 0
+
+ge_image_step dd 0
 
 graphic_editor:
  DRAW_WINDOW graphic_editor_up_str, graphic_editor_down_str, 0xFFAE29, 0xBBBBBB
@@ -66,6 +75,12 @@ graphic_editor:
   cmp byte [key_code], KEY_F3
   je .new_file
   
+  cmp byte [key_code], KEY_F4
+  je .step_back
+  
+  cmp byte [key_code], KEY_F5
+  je .step_forward
+  
   cmp byte [key_code], KEY_UP
   je .key_up
   
@@ -89,6 +104,18 @@ graphic_editor:
   
   cmp byte [key_code], KEY_D
   je .tool_square
+  
+  cmp byte [key_code], KEY_F
+  je .tool_circle
+  
+  cmp byte [key_code], KEY_G
+  je .tool_fill
+  
+  cmp byte [key_code], KEY_1
+  je .empty_tool
+  
+  cmp byte [key_code], KEY_2
+  je .full_tool
   
   cmp dword [usb_mouse_data], 0
   jne .move_mouse
@@ -202,6 +229,9 @@ graphic_editor:
   inc eax
   mov dword [allocated_size], eax
   call release_memory
+  mov eax, dword [ge_image_step_back_pointer]
+  mov dword [allocated_memory_pointer], eax
+  call release_memory
   
   mov dword [ge_mouse_img_line], 20
   mov dword [ge_mouse_img_column], 0
@@ -210,10 +240,20 @@ graphic_editor:
   jne .if_resoultion_a
    mov dword [lc_image_width], 100
    mov dword [lc_image_heigth], 100
+   
    mov dword [allocated_size], 1
    call allocate_memory
    mov eax, dword [allocated_memory_pointer]
    mov dword [file_memory], eax
+   mov edi, eax
+   mov eax, WHITE
+   mov ecx, 100*100+2
+   rep stosd
+   
+   mov dword [allocated_size], 1
+   call allocate_memory
+   mov eax, dword [allocated_memory_pointer]
+   mov dword [ge_image_step_back_pointer], eax
    mov edi, eax
    mov eax, WHITE
    mov ecx, 100*100+2
@@ -225,10 +265,20 @@ graphic_editor:
   jne .if_resoultion_b
    mov dword [lc_image_width], 640
    mov dword [lc_image_heigth], 480
+   
    mov dword [allocated_size], 2
    call allocate_memory
    mov eax, dword [allocated_memory_pointer]
    mov dword [file_memory], eax
+   mov edi, eax
+   mov eax, WHITE
+   mov ecx, 640*480+2
+   rep stosd
+   
+   mov dword [allocated_size], 2
+   call allocate_memory
+   mov eax, dword [allocated_memory_pointer]
+   mov dword [ge_image_step_back_pointer], eax
    mov edi, eax
    mov eax, WHITE
    mov ecx, 640*480+2
@@ -240,10 +290,20 @@ graphic_editor:
   jne .if_resoultion_c
    mov dword [lc_image_width], 800
    mov dword [lc_image_heigth], 600
+   
    mov dword [allocated_size], 2
    call allocate_memory
    mov eax, dword [allocated_memory_pointer]
    mov dword [file_memory], eax
+   mov edi, eax
+   mov eax, WHITE
+   mov ecx, 800*600+2
+   rep stosd
+   
+   mov dword [allocated_size], 2
+   call allocate_memory
+   mov eax, dword [allocated_memory_pointer]
+   mov dword [ge_image_step_back_pointer], eax
    mov edi, eax
    mov eax, WHITE
    mov ecx, 800*600+2
@@ -255,6 +315,7 @@ graphic_editor:
   jne .if_resoultion_d
    mov dword [lc_image_width], 1024
    mov dword [lc_image_heigth], 768
+   
    mov dword [allocated_size], 3
    call allocate_memory
    mov eax, dword [allocated_memory_pointer]
@@ -263,8 +324,31 @@ graphic_editor:
    mov eax, WHITE
    mov ecx, 1024*768+2
    rep stosd
+   
+   mov dword [allocated_size], 3
+   call allocate_memory
+   mov eax, dword [allocated_memory_pointer]
+   mov dword [ge_image_step_back_pointer], eax
+   mov edi, eax
+   mov eax, WHITE
+   mov ecx, 1024*768+2
+   rep stosd
    jmp .set_new_image_variabiles
   .if_resoultion_d:
+ jmp graphic_editor
+ 
+ .step_back:
+  mov eax, dword [ge_image_pointer]
+  mov ebx, dword [ge_image_step_back_pointer]
+  mov dword [ge_image_pointer], ebx
+  mov dword [ge_image_step_back_pointer], eax
+ jmp graphic_editor
+ 
+ .step_forward:
+  mov eax, dword [ge_image_pointer]
+  mov ebx, dword [ge_image_step_back_pointer]
+  mov dword [ge_image_pointer], ebx
+  mov dword [ge_image_step_back_pointer], eax
  jmp graphic_editor
   
  .key_up:
@@ -364,6 +448,50 @@ graphic_editor:
   call redraw_screen
  jmp .graphic_editor_halt
  
+ .tool_circle:
+  mov dword [ge_draw_object], GE_CIRCLE
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .tool_fill:
+  mov dword [ge_draw_object], GE_FILL
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .empty_tool:
+  mov dword [ge_tool_option], GE_EMPTY
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .full_tool:
+  mov dword [ge_tool_option], GE_FULL
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
  .mouse_end_of_click:
   cmp dword [ge_drawing_state], 0
   je .graphic_editor_halt
@@ -373,6 +501,18 @@ graphic_editor:
    mov dword [ge_drawing_state], 0
    jmp .graphic_editor_halt
   .end_of_click_if_free_draw:
+  
+  cmp dword [ge_draw_object], GE_FILL
+  je .graphic_editor_halt
+  
+  ;save previous state
+  mov esi, dword [ge_image_pointer]
+  mov edi, dword [ge_image_step_back_pointer]
+  mov eax, dword [ge_image_width]
+  mov ebx, dword [ge_image_heigth]
+  mul ebx
+  mov ecx, eax
+  rep movsd
   
   push dword [screen_mem_pointer]
   push dword [screen_x]
@@ -415,15 +555,61 @@ graphic_editor:
  .mouse_click:
   mov dword [ge_drawing_state], 1
   
+  cmp dword [ge_draw_object], GE_FREE_DRAW
+  je .mouse_click_save_state
+  cmp dword [ge_draw_object], GE_FILL
+  jne .mouse_click_if_free_draw
+  .mouse_click_save_state:
+   ;save previous state
+   mov esi, dword [ge_image_pointer]
+   mov edi, dword [ge_image_step_back_pointer]
+   mov eax, dword [ge_image_width]
+   mov ebx, dword [ge_image_heigth]
+   mul ebx
+   mov ecx, eax
+   rep movsd
+  .mouse_click_if_free_draw:
+  
   mov eax, dword [cursor_line]
   mov dword [ge_draw_click1_line], eax
   mov dword [ge_draw_click2_line], eax
   mov eax, dword [cursor_column]
   mov dword [ge_draw_click1_column], eax
   mov dword [ge_draw_click2_column], eax
+  
+  cmp dword [ge_draw_object], GE_FILL
+  jne .if_mouse_click_fill
+   push dword [screen_mem_pointer]
+   push dword [screen_x]
+   push dword [screen_y]
+   push dword [screen_pixels_per_line]
+   mov eax, dword [ge_image_pointer]
+   mov dword [screen_mem_pointer], eax
+   mov eax, dword [ge_image_width]
+   mov dword [screen_x], eax
+   mov eax, dword [ge_image_heigth]
+   mov dword [screen_y], eax
+   mov eax, dword [ge_image_bytes_per_line]
+   mov dword [screen_pixels_per_line], eax
+  
+   mov eax, dword [ge_selected_color]
+   mov dword [color], eax
+   
+   call ge_draw_object_to_memory
+   
+   pop dword [screen_pixels_per_line]
+   pop dword [screen_y]
+   pop dword [screen_x]
+   pop dword [screen_mem_pointer]
+   
+   jmp graphic_editor
+  .if_mouse_click_fill
  jmp .graphic_editor_halt
  
  .mouse_drag_and_drop:
+  cmp dword [ge_draw_object], GE_FILL
+  je .graphic_editor_halt
+ 
   mov eax, dword [cursor_line]
   mov dword [ge_draw_click2_line], eax
   mov eax, dword [cursor_column]
@@ -433,7 +619,7 @@ graphic_editor:
   push dword [cursor_column]
   
   cmp dword [ge_draw_object], GE_FREE_DRAW
-  jne .drag_and_drop_if_free_draw
+  jne .drag_and_drop_if_free_draw 
    push dword [screen_mem_pointer]
    push dword [screen_x]
    push dword [screen_y]
@@ -523,7 +709,7 @@ ge_draw_object_to_memory:
   call draw_line_all
   jmp .done
  .if_draw_free:
-  
+ 
  cmp dword [ge_draw_object], GE_SQUARE
  jne .if_draw_square
   mov eax, dword [ge_draw_click1_line]
@@ -567,16 +753,90 @@ ge_draw_object_to_memory:
   inc eax
   mov dword [square_length], eax
   
-  call draw_square
+  mov eax, dword [ge_tool_option]
+  cmp eax, GE_EMPTY
+  je .empty_square
+  
+  call draw_square ;full square
+  jmp .done
+  
+  .empty_square:
+  call draw_empty_square
   jmp .done
  .if_draw_square:
  
+ cmp dword [ge_draw_object], GE_CIRCLE
+ jne .if_draw_circle
+  mov eax, dword [ge_draw_click1_line]
+  mov ebx, dword [ge_draw_click2_line]
+  mov ecx, ebx
+  sub ecx, eax
+  shr ecx, 1 ;div 2
+  mov dword [ellipse_y_radius], ecx
+  mov dword [cursor_line], eax
+  add dword [cursor_line], ecx
+  cmp eax, ebx
+  jb .if_circle_line
+   mov ecx, eax
+   sub ecx, ebx
+   shr ecx, 1 ;div 2
+   mov dword [ellipse_y_radius], ecx
+   add ebx, ecx
+   mov dword [cursor_line], ebx
+  .if_circle_line:
+  
+  mov eax, dword [ge_draw_click1_column]
+  mov ebx, dword [ge_draw_click2_column]
+  mov ecx, ebx
+  sub ecx, eax
+  shr ecx, 1 ;div 2
+  mov dword [ellipse_x_radius], ecx
+  mov dword [cursor_column], eax
+  add dword [cursor_column], ecx
+  cmp eax, ebx
+  jb .if_circle_column
+   mov ecx, eax
+   sub ecx, ebx
+   shr ecx, 1 ;div 2
+   mov dword [ellipse_x_radius], ecx
+   add ebx, ecx
+   mov dword [cursor_column], ebx
+  .if_circle_column:
+  
+  sub dword [cursor_line], 20
+  
+  mov eax, dword [ge_tool_option]
+  cmp eax, GE_EMPTY
+  je .empty_ellipse
+  
+  call draw_ellipse ;full ellipse
+  jmp .done
+  
+  .empty_ellipse:
+  call draw_empty_ellipse
+  jmp .done
+ .if_draw_circle:
+ 
+ cmp dword [ge_draw_object], GE_FILL
+ jne .if_fill
+  mov eax, dword [ge_draw_click1_line]
+  sub eax, 20
+  mov dword [cursor_line], eax
+  mov eax, dword [ge_draw_click1_column]
+  mov dword [cursor_column], eax
+  call fill_area
+  jmp .done
+ .if_fill:
+ 
  .done:
  pop dword [ge_draw_click2_column]
- pop dword [ge_draw_click2_line]
+ pop dword [ge_draw_click2_line] 
  ret
  
 ge_draw_object_on_screen:
+ push dword [ge_draw_click2_line]
+ push dword [ge_draw_click2_column]
+ 
  cmp dword [ge_draw_object], GE_FREE_DRAW
  je .draw_line
  cmp dword [ge_draw_object], GE_LINE_DRAW
@@ -591,9 +851,9 @@ ge_draw_object_on_screen:
   mov eax, dword [ge_draw_click2_column]
   mov dword [x2], eax
   call draw_line_all
-  ret
+  jmp .done
  .if_draw_free:
-  
+ 
  cmp dword [ge_draw_object], GE_SQUARE
  jne .if_draw_square
   mov eax, dword [ge_draw_click1_line]
@@ -636,10 +896,71 @@ ge_draw_object_on_screen:
   inc eax
   mov dword [square_length], eax
   
-  call draw_square
-  ret
+  mov eax, dword [ge_tool_option]
+  cmp eax, GE_EMPTY
+  je .empty_square
+  
+  call draw_square ;full square
+  jmp .done
+  
+  .empty_square:
+  call draw_empty_square
+  jmp .done
  .if_draw_square:
  
+ cmp dword [ge_draw_object], GE_CIRCLE
+ jne .if_draw_circle
+  mov eax, dword [ge_draw_click1_line]
+  mov ebx, dword [ge_draw_click2_line]
+  mov ecx, ebx
+  sub ecx, eax
+  shr ecx, 1 ;div 2
+  mov dword [ellipse_y_radius], ecx
+  mov dword [cursor_line], eax
+  add dword [cursor_line], ecx
+  cmp eax, ebx
+  jb .if_circle_line
+   mov ecx, eax
+   sub ecx, ebx
+   shr ecx, 1 ;div 2
+   mov dword [ellipse_y_radius], ecx
+   add ebx, ecx
+   mov dword [cursor_line], ebx
+  .if_circle_line:
+  
+  mov eax, dword [ge_draw_click1_column]
+  mov ebx, dword [ge_draw_click2_column]
+  mov ecx, ebx
+  sub ecx, eax
+  shr ecx, 1 ;div 2
+  mov dword [ellipse_x_radius], ecx
+  mov dword [cursor_column], eax
+  add dword [cursor_column], ecx
+  cmp eax, ebx
+  jb .if_circle_column
+   mov ecx, eax
+   sub ecx, ebx
+   shr ecx, 1 ;div 2
+   mov dword [ellipse_x_radius], ecx
+   add ebx, ecx
+   mov dword [cursor_column], ebx
+  .if_circle_column:
+  
+  mov eax, dword [ge_tool_option]
+  cmp eax, GE_EMPTY
+  je .empty_ellipse
+  
+  call draw_ellipse ;full ellipse
+  jmp .done
+  
+  .empty_ellipse:
+  call draw_empty_ellipse
+  jmp .done
+ .if_draw_circle:
+ 
+ .done:
+ pop dword [ge_draw_click2_column]
+ pop dword [ge_draw_click2_line]
  ret
 
 ge_select_color:
@@ -669,6 +990,18 @@ ge_select_color:
   
   cmp byte [key_code], KEY_D
   je .tool_square
+  
+  cmp byte [key_code], KEY_F
+  je .tool_circle
+  
+  cmp byte [key_code], KEY_G
+  je .tool_fill
+  
+  cmp byte [key_code], KEY_1
+  je .empty_tool
+  
+  cmp byte [key_code], KEY_2
+  je .full_tool
   
   cmp dword [usb_mouse_dnd], 0x1
   je .mouse_click
@@ -702,6 +1035,50 @@ ge_select_color:
   call draw_cursor
   call redraw_screen
  jmp .ge_select_color_halt
+ 
+ .tool_circle:
+  mov dword [ge_draw_object], GE_CIRCLE
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .tool_fill:
+  mov dword [ge_draw_object], GE_FILL
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .empty_tool:
+  mov dword [ge_tool_option], GE_EMPTY
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
+ 
+ .full_tool:
+  mov dword [ge_tool_option], GE_FULL
+  mov dword [ge_drawing_state], 0
+  push dword [cursor_line]
+  push dword [cursor_column]
+  call graphic_editor_draw_panel
+  pop dword [cursor_column]
+  pop dword [cursor_line]
+  call redraw_screen
+ jmp .graphic_editor_halt
  
  .move_mouse:
   mov eax, dword [ge_mouse_line]
