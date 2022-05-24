f79a0dc3de18ae7e5eb5bb324a4cff98c944f395
Klaykap <43180618+Klaykap@users.noreply.github.com>
2022-05-24 11:44:31 +0200

    Update board_developer_zone.asm

--- source/gui/board_developer_zone.asm
+++ source/gui/board_developer_zone.asm
@@ -9,11 +9,8 @@ developer_zone:
  ;text
  mov dword [color], BLACK
  PRINT 'Developer zone', dz_up_str, LINE(1), COLUMN(1)
- PRINT '[a] Hardware info', dz_hw_str, LINE(3), COLUMN(1)
- PRINT '[b] Intel HD Audio', dz_hda_str, LINE(5), COLUMN(1)
- PRINT '[c] IDE', dz_ide_str, LINE(7), COLUMN(1)
- PRINT '[d] Mouse', dz_mouse_str, LINE(9), COLUMN(1)
- PRINT '[e] Acpi', dz_acpi_str, LINE(11), COLUMN(1)
+ PRINT '[a] Log viewer', dz_log_viewer_str, LINE(3), COLUMN(1)
+ PRINT '[b] Mouse', dz_mouse_str, LINE(5), COLUMN(1)
 
  call redraw_screen
 
@@ -24,17 +21,8 @@ developer_zone:
   je main_window
 
   cmp byte [key_code], KEY_A
-  je hardware_info
+  je log_viewer
 
   cmp byte [key_code], KEY_B
-  je dp_hda
-
-  cmp byte [key_code], KEY_C
-  je dp_ide
-
-  cmp byte [key_code], KEY_D
   je dp_mouse
-
-  cmp byte [key_code], KEY_E
-  je dp_acpi
  jmp .developer_zone_halt
