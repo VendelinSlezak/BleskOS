49c16e26a484838fea32ac367cbc990f45e6a6e0
Vendelín Slezák <43180618+VendelinSlezak@users.noreply.github.com>
2023-09-28 17:11:03 +0200

    Add files via upload

--- source/drivers/ahci/ahci_controller.h
+++ source/drivers/ahci/ahci_controller.h
@@ -8,6 +8,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "ahci_controller.c"
-#include "ahci_serial_ata.c"
-#include "ahci_serial_atapi.c"
+dword_t ahci_controllers[10];
+dword_t ahci_ports[32][2];
+
+dword_t ahci_controllers_pointer = 0, ahci_ports_pointer = 0;
+dword_t ahci_base, ahci_drive_size, ahci_hdd_base, ahci_hdd_size, ahci_cdrom_base, ahci_cdrom_size;
+dword_t ahci_hdd_cmd_mem, ahci_hdd_receive_fis_mem, ahci_hdd_fis_mem, ahci_cdrom_cmd_mem, ahci_cdrom_receive_fis_mem, ahci_cdrom_fis_mem;
+
+void initalize_ahci_controllers(void);
+void initalize_ahci_port(dword_t base_port, dword_t commands_memory, dword_t receive_fis_memory);
--- source/drivers/ahci/ahci_serial_ata.h
+++ source/drivers/ahci/ahci_serial_ata.h
@@ -8,6 +8,10 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "ahci_controller.c"
-#include "ahci_serial_ata.c"
-#include "ahci_serial_atapi.c"
+dword_t ahci_drive_info;
+
+byte_t sata_wait_for_drive(dword_t base_port, dword_t wait_ticks);
+byte_t sata_send_command(dword_t base_port, dword_t commands_memory, dword_t fis_memory, byte_t command, dword_t sector, byte_t number_of_sectors, dword_t transfer_length, dword_t memory);
+byte_t sata_read_drive_info(dword_t base_port, dword_t commands_memory, dword_t fis_memory);
+byte_t sata_read(dword_t base_port, dword_t commands_memory, dword_t fis_memory, dword_t sector, byte_t number_of_sectors, dword_t memory);
+byte_t sata_write(dword_t base_port, dword_t commands_memory, dword_t fis_memory, dword_t sector, byte_t number_of_sectors, dword_t memory);
