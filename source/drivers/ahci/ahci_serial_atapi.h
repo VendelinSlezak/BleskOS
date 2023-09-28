607f3c34b47d8adeb907f24ff4f3ce348371faab
Vendelín Slezák <43180618+VendelinSlezak@users.noreply.github.com>
2023-09-28 17:11:03 +0200

    Add files via upload

--- source/drivers/ahci/ahci_serial_atapi.h
+++ source/drivers/ahci/ahci_serial_atapi.h
@@ -8,6 +8,8 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "ahci_controller.c"
-#include "ahci_serial_ata.c"
-#include "ahci_serial_atapi.c"
+byte_t satapi_send_command(dword_t base_port, dword_t commands_memory, dword_t fis_memory, dword_t transfer_length, dword_t cmd1, dword_t cmd2, dword_t cmd3, dword_t memory);
+byte_t satapi_eject_drive(dword_t base_port, dword_t commands_memory, dword_t fis_memory);
+byte_t satapi_detect_disk(dword_t base_port, dword_t commands_memory, dword_t fis_memory);
+byte_t satapi_read_drive_capabilites(dword_t base_port, dword_t commands_memory, dword_t fis_memory);
+byte_t satapi_read_sector(dword_t base_port, dword_t commands_memory, dword_t fis_memory, dword_t sector, dword_t memory);
