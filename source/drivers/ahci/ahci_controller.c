0cd59ec9b206ba898cd49bda9e298d6a06cd90ea
Vendelín Slezák <43180618+VendelinSlezak@users.noreply.github.com>
2023-10-02 19:46:39 +0200

    Add files via upload

--- source/drivers/ahci/ahci_controller.c
+++ source/drivers/ahci/ahci_controller.c
@@ -9,8 +9,9 @@
 */
 
 void initalize_ahci_controllers(void) {
- ahci_hdd_base=0;
- ahci_cdrom_base=0;
+ ahci_hdd_base = 0;
+ ahci_cdrom_base = 0;
+ ahci_ports_pointer = 0;
 
  if(ahci_controllers_pointer==0) {
   log("\nno AHCI controller founded\n");
@@ -18,12 +19,15 @@ void initalize_ahci_controllers(void) {
  }
  
  log("\n");
- log_var(ahci_controllers_pointer);
+ log_var_with_space(ahci_controllers_pointer);
  log("AHCI controllers\n");
 
  //initalize controllers
  for(int i=0; i<ahci_controllers_pointer; i++) {
   ahci_base = ahci_controllers[i];
+
+  //disable interrupts
+  mmio_outd(ahci_base + 0x04, 0x80000000);
 
   //disable BIOS ownership
   if(mmio_ind(ahci_base + 0x10)>=0x00010200) { //in lower version this is not supported
@@ -31,7 +35,7 @@ void initalize_ahci_controllers(void) {
     mmio_outd(ahci_base + 0x28, 0x2); //we want to take ownership
     
     ticks=0;
-    while(ticks<1000) {
+    while(ticks<500) {
      if((mmio_ind(ahci_base + 0x28) & 0x3)==0x2) {
       break; //we have ownership
      }
@@ -40,24 +44,44 @@ void initalize_ahci_controllers(void) {
     continue;
    }
   }
+
+  //scan for ports
+  for(int port_num=0, mask=1; port_num<32; port_num++, mask<<=1) {
+   if(ahci_ports_pointer>=32) {
+    return;
+   }
+   
+   //test if port exist
+   if((mmio_ind(ahci_base + 0x0C) & mask)==mask) {
+    ahci_ports[ahci_ports_pointer][0]=(ahci_base + 0x100 + (port_num*0x80)); //save port base address
+    ahci_ports_pointer++;
+   }
+  }
  }
  
- log_var(ahci_ports_pointer);
+ log_var_with_space(ahci_ports_pointer);
  log("AHCI ports\n");
 
  if(ahci_ports_pointer==0) {
   return; //no AHCI ports
  }
- 
+
  //allocate memory for AHCI ports
- ahci_hdd_cmd_mem = aligned_malloc(32*32, 0x3FF);
- ahci_cdrom_cmd_mem = aligned_malloc(32*32, 0x3FF);
- ahci_hdd_receive_fis_mem = aligned_malloc(32*32, 0xFF);
- ahci_cdrom_receive_fis_mem = aligned_malloc(32*32, 0xFF);
+ ahci_hdd_cmd_mem = aligned_calloc(32*32, 0x3FF);
+ ahci_cdrom_cmd_mem = aligned_calloc(32*32, 0x3FF);
+ ahci_hdd_receive_fis_mem = aligned_calloc(32*32, 0xFF);
+ ahci_cdrom_receive_fis_mem = aligned_calloc(32*32, 0xFF);
  ahci_hdd_fis_mem = calloc(0x90);
  ahci_cdrom_fis_mem = calloc(0x90);
- 
  ahci_drive_info = malloc(512);
+
+ //read port signatures
+ for(dword_t i=0; i<ahci_ports_pointer; i++) {
+  //TODO: reset port
+  
+  //read signature
+  ahci_ports[i][1] = mmio_ind(ahci_ports[i][0] + 0x24);
+ }
 
  //find hard disk
  for(int i=0; i<ahci_ports_pointer; i++) {
@@ -86,18 +110,21 @@ void initalize_ahci_controllers(void) {
   log_hex(ahci_ports[i][1]);
   log("\n");
  }
- log("AHCI HDD size: ");
- log_var(ahci_hdd_size);
- log("\n");
+ if(ahci_hdd_base!=0) {
+  log("AHCI HDD size: ");
+  log_var(ahci_hdd_size);
+  log("\n");
+ }
 }
 
 void initalize_ahci_port(dword_t base_port, dword_t commands_memory, dword_t receive_fis_memory) {
  //stop port
- mmio_outd(base_port + 0x18, 0x0);
+ mmio_outd(base_port + 0x18, 0x6);
  wait(100);
  
  //disable interrupts
  mmio_outd(base_port + 0x14, 0x0);
+ mmio_outd(base_port + 0x10, 0xFFFFFFFF);
  
  //set memory
  mmio_outd(base_port + 0x00, commands_memory);
@@ -109,5 +136,8 @@ void initalize_ahci_port(dword_t base_port, dword_t commands_memory, dword_t rec
  mmio_outd(base_port + 0x40, 0x0);
  
  //start port
- mmio_outd(base_port + 0x18, 0x11);
+ mmio_outd(base_port + 0x18, 0x16);
+ wait(5);
+ mmio_outd(base_port + 0x18, 0x17);
+ wait(5);
 }
