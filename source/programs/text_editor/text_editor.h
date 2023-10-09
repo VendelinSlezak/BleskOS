ceeb0a205b3fe93b33ee76559cf06105e2102804
Vendelín Slezák <43180618+VendelinSlezak@users.noreply.github.com>
2023-10-09 15:17:27 +0200

    Add files via upload

--- source/programs/text_editor/text_editor.h
+++ source/programs/text_editor/text_editor.h
@@ -13,11 +13,23 @@
 #define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA 100
 #define TEXT_EDITOR_CLICK_ZONE_VERTICAL_SCROLLBAR 101
 #define TEXT_EDITOR_CLICK_ZONE_HORIZONTAL_SCROLLBAR 102
-#define TEXT_EDITOR_CLICK_ZONE_UNDO 103
-#define TEXT_EDITOR_CLICK_ZONE_REDO 104
-#define TEXT_EDITOR_CLICK_ZONE_SWITCH_COLOR_MODE 105
+#define TEXT_EDITOR_CLICK_ZONE_MORE 103
+#define TEXT_EDITOR_CLICK_ZONE_UNDO 104
+#define TEXT_EDITOR_CLICK_ZONE_REDO 105
+#define TEXT_EDITOR_CLICK_ZONE_MORE_LIST 106
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_FIND 107
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_FIND_NEXT 108
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_FIND_PREVIOUS 109
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_REPLACE 110
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_REPLACE_ACTUAL_SELECTION 111
+#define TEXT_EDITOR_CLICK_ZONE_TEXT_AREA_REPLACE_ALL 112
 
-dword_t text_editor_program_interface_memory = 0, text_editor_button_undo_on_screen = 0, text_editor_button_redo_on_screen = 0;
+#define TEXT_EDITOR_STATE_TEXT 1
+#define TEXT_EDITOR_STATE_FIND 2
+#define TEXT_EDITOR_STATE_FIND_AND_REPLACE 3
+
+dword_t text_editor_program_interface_memory = 0, text_editor_button_undo_on_screen = 0, text_editor_button_redo_on_screen = 0, text_editor_state = 0, text_editor_more_list_on_screen = 0;
+dword_t text_editor_find_function_text_area_info_mem = 0, text_editor_replace_function_text_area_mem = 0, text_editor_go_to_line_text_area_mem = 0;
 
 void initalize_text_editor(void);
 void text_editor(void);
@@ -26,6 +38,18 @@ void text_editor_open_file(void);
 void text_editor_save_file(void);
 void text_editor_new_file(void);
 void text_editor_close_file(void);
-void text_editor_key_F7_event(void);
+void text_editor_key_f7_event(void);
+void text_editor_key_f8_event(void);
+void text_editor_key_f9_event(void);
+void text_editor_key_f10_event(void);
+void text_editor_key_f11_event(void);
 void text_editor_click_zone_undo_event(void);
 void text_editor_click_zone_redo_event(void);
+void text_editor_click_zone_more_list(void);
+void text_area_unfocus_find_and_replace(void);
+void text_area_unfocus_find(void);
+void text_area_unfocus_replace(void);
+void text_editor_find_next(void);
+void text_editor_find_previous(void);
+void text_editor_replace_actual_selection(void);
+void text_editor_replace_all(void);
