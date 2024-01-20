6a9d93b9d9d6691958b3cd9bc2f91454bd39af7b
defdefred <defdefred@protonmail.com>
2024-01-20 14:48:05 +0100

    performance_rating more task

--- source/programs/performance_rating/performance_rating.h
+++ source/programs/performance_rating/performance_rating.h
@@ -13,6 +13,7 @@
 #define PERFORMANCE_RATING_RUN_COUNT 128
 #define PERFORMANCE_RATING_NBTASK 16
 
+dword_t PERFORMANCE_RATING_NBTASK_PER_PAGE=0;
 dword_t PERFORMANCE_RATING_CURRENT_TASK=0;
 dword_t PERFORMANCE_RATING_CURRENT_PAGE=0;
 dword_t PERFORMANCE_RATING_CURRENT_RUN=0;
@@ -39,7 +40,7 @@ struct performance_rating_task { dword_t result; void (*run)(void); byte_t name[
 	{ 0, &performance_rating_task1, "Mouse move 100% visible" },
 	{ 0, &performance_rating_task2, "Mouse move 50% visible right" },
 	{ 0, &performance_rating_task3, "Mouse move 50% visible bottom" },
-	{ 0, &performance_rating_task4, "Redraw screen from double buffer to framebuffer 32, 24 or 16bpp" },
+	{ 0, &performance_rating_task4, "Redraw screen from double buffer to framebuffer" },
 	{ 0, &performance_rating_task5, "Redraw 25% of screen 100% visible" },
 	{ 0, &performance_rating_task6, "Redraw 25% of screen 50% right visible" },
 	{ 0, &performance_rating_task7, "Redraw 25% of screen 50% bottom visible" },
