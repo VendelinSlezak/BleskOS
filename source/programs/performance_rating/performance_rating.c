8ea9e6dd78fec7996ece15e7f5e62154757223f6
defdefred <defdefred@protonmail.com>
2024-02-04 23:41:04 +0100

    performance_rating more task

--- source/programs/performance_rating/performance_rating.c
+++ source/programs/performance_rating/performance_rating.c
@@ -14,6 +14,8 @@ void initalize_performance_rating(void) {
 
 void performance_rating(void) {
   redraw_performance_rating();
+  set_pen_width(1);
+  global_color=WHITE;
 
   while(1) {
     wait_for_user_input();
@@ -42,9 +44,9 @@ void performance_rating(void) {
         redraw_performance_rating();
         break;
       case KEY_A:
-        PERFORMANCE_RATING_CURRENT_TASK=0;
-        redraw_performance_rating();
-        for (; PERFORMANCE_RATING_CURRENT_TASK<PERFORMANCE_RATING_NBTASK; PERFORMANCE_RATING_CURRENT_TASK++) {
+        for (PERFORMANCE_RATING_CURRENT_TASK=0; PERFORMANCE_RATING_CURRENT_TASK<PERFORMANCE_RATING_NBTASK; PERFORMANCE_RATING_CURRENT_TASK++) {
+          performance_rating_tasks[PERFORMANCE_RATING_CURRENT_TASK].result = 0;
+          redraw_performance_rating();
           performance_rating_run_task(PERFORMANCE_RATING_CURRENT_TASK);
           redraw_performance_rating();
 			 if( keyboard_value == KEY_ESC ) {break;}
@@ -55,7 +57,8 @@ void performance_rating(void) {
         redraw_performance_rating();
         break;
       case KEY_PAGE_DOWN:
-        if (PERFORMANCE_RATING_CURRENT_TASK < PERFORMANCE_RATING_NBTASK-5) { PERFORMANCE_RATING_CURRENT_TASK+=5; } else { PERFORMANCE_RATING_CURRENT_TASK=PERFORMANCE_RATING_NBTASK-1; }
+        if (PERFORMANCE_RATING_CURRENT_TASK < PERFORMANCE_RATING_NBTASK-5) { PERFORMANCE_RATING_CURRENT_TASK+=5; }
+          else { PERFORMANCE_RATING_CURRENT_TASK=PERFORMANCE_RATING_NBTASK-1; }
         redraw_performance_rating();
         break;
       case KEY_ESC:
@@ -109,17 +112,17 @@ void performance_rating_run_task(dword_t task_number) {  // get time of 128 runs
   performance_rating_tasks[task_number].result = get_timer_value_in_microseconds();
 }
 
-void performance_rating_inspect_task(dword_t task_number) {  // get time of 128 runs
-  message_window("Running tasks, [ESC] to quit...");
-  redraw_screen();
+void performance_rating_inspect_task(dword_t task_number) {  // to be sure what's going on
   mouse_movement_x = graphic_screen_x - mouse_cursor_x - (MOUSE_CURSOR_WIDTH>>1);
   mouse_movement_y = graphic_screen_y - mouse_cursor_y - (MOUSE_CURSOR_HEIGTH>>1);
   move_mouse_cursor(); // mouse need to be on expected position
 
   for(PERFORMANCE_RATING_CURRENT_RUN=0; PERFORMANCE_RATING_CURRENT_RUN<PERFORMANCE_RATING_RUN_COUNT; PERFORMANCE_RATING_CURRENT_RUN++) {
+    clear_screen(PERFORMANCE_RATING_CURRENT_RUN<<1);
     performance_rating_tasks[task_number].run();
-    draw_full_square(0, 0, 32, 10, BLACK);
-    print_var(PERFORMANCE_RATING_CURRENT_RUN,2,2,WHITE);
+    draw_full_square(0, 0, 64, 10, BLACK);
+    print("[ESC]",2,2,WHITE);
+    print_var(PERFORMANCE_RATING_CURRENT_RUN,42,2,WHITE);
     redraw_screen();
     if (keyboard_value == KEY_ESC) { break;}
   }
@@ -144,89 +147,88 @@ void performance_rating_task3() { //on screen border bottom
   mouse_movement_x = -3;
   move_mouse_cursor();
 }
-
 void performance_rating_task4() { // full screen
   redraw_screen();
 }
-
-void performance_rating_task5() { // 25% screen center
-  redraw_part_of_framebuffer( graphic_screen_x_center>>1, graphic_screen_y_center>>1, graphic_screen_x>>1, graphic_screen_y>>1);
+void performance_rating_task5() { // 25% screen
+  redraw_part_of_screen( graphic_screen_x_center>>1, graphic_screen_y_center>>1, graphic_screen_x>>1, graphic_screen_y>>1);
 }
-
 void performance_rating_task6() { // 25% screen half rigth
-  redraw_part_of_framebuffer( graphic_screen_x_center+(graphic_screen_x_center>>1), graphic_screen_y_center>>1, graphic_screen_x>>1, graphic_screen_y>>1);
+  redraw_part_of_screen( graphic_screen_x_center+(graphic_screen_x_center>>1), graphic_screen_y_center>>1, graphic_screen_x>>1, graphic_screen_y>>1);
 }
-
 void performance_rating_task7() { // 25% screen half bottom
-  redraw_part_of_framebuffer( graphic_screen_x_center>>1, graphic_screen_y_center+(graphic_screen_y_center>>1), graphic_screen_x>>1, graphic_screen_y>>1);
+  redraw_part_of_screen( graphic_screen_x_center>>1, graphic_screen_y_center+(graphic_screen_y_center>>1), graphic_screen_x>>1, graphic_screen_y>>1);
 }
-
 void performance_rating_task8() { //  Scalable font print
-  scalable_font_print("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  scalable_font_print("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task9() { // bitmap font print
-  print("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  print("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task10() { // print numeric variable 
-  print_var(3473898+(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  print_var(3473898+(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task11() { // print unicode string ÀÁÂÃÄÅÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝßàáâãäåèéêëìíîïñòóôõöùúûüýÿčďěľňŕřšťž
-  print_unicode((word_t *)"\u00c0\u00c1\u00c2\u00c3\u00c4\u00c5\u00c8\u00c9\u00ca\u00cb\u00cc\u00cd\u00ce\u00cf\u00d1\u00d2\u00d3\u00d4\u00d5\u00d6\u00d9\u00da\u00db\u00dc\u00dd\u00df\u00e0\u00e1\u00e2\u00e3\u00e4\u00e5\u00e8\u00e9\u00ea\u00eb\u00ec\u00ed\u00ee\u00ef\u00f1\u00f2\u00f3\u00f4\u00f5\u00f6\u00f9\u00fa\u00fb\u00fc\u00fd\u00ff\u010d\u010f\u011b\u013e\u0148\u0155\u0159\u0161\u0165\u017e",
-    graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  print_unicode((word_t *)"\xc0\x00\xc1\x00\xc2\x00\xc3\x00\xc4\x00\xc5\x00\xc8\x00\xc9\x00\xca\x00\xcb\x00\xcc\x00\xcd\x00\xce\x00\xcf\x00\xd1\x00\xd2\x00\xd3\x00\xd4\x00\xd5\x00\xd6\x00\xd9\x00\xda\x00\xdb\x00\xdc\x00\xdd\x00\xdf\x00\xe0\x00\xe1\x00\xe2\x00\xe3\x00\xe4\x00\xe5\x00\xe8\x00\xe9\x00\xea\x00\xeb\x00\xec\x00\xed\x00\xee\x00\xef\x00\xf1\x00\xf2\x00\xf3\x00\xf4\x00\xf5\x00\xf6\x00\xf9\x00\xfa\x00\xfb\x00\xfc\x00\xfd\x00\xff\x00\x0d\x01\x0f\x01\x1b\x01\x3e\x01\x48\x01\x55\x01\x59\x01\x61\x01\x65\x01\x7e\x01\x00\x00",
+    graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task12() { // print hexa number
-  print_hex(3467423+(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  print_hex(3467423+(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task13() { // print ascii
-  print_ascii("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), BLACK);  
+  print_ascii("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789", graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);  
 }
 void performance_rating_task14() { // draw pixel
-  draw_pixel(graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1));
+  global_color=WHITE;
+  draw_pixel(graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1));
 }
 void performance_rating_task15() { // draw straigth line
-  draw_straigth_line(graphic_screen_x_center>>1, graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_x_center );
+  draw_straigth_line(graphic_screen_x_center>>1, graphic_screen_y_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_x_center );
 }
 void performance_rating_task16() { // draw straigth column
-  draw_straigth_column(graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_y_center>>1, graphic_screen_y_center);
+  draw_straigth_column(graphic_screen_x_center-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_y_center>>1, graphic_screen_y_center);
 }
 void performance_rating_task17() { // draw line
-  draw_line(0,(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x, graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<2), BLACK);
+  draw_line(0,(PERFORMANCE_RATING_CURRENT_RUN<<2), graphic_screen_x, graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<2), WHITE);
 }
 void performance_rating_task18() { // draw quadratic bezier
-//  draw_quadratic_bezier(int x0, int y0, int x1, int y1, int x2, int y2, dword_t color);
+  draw_quadratic_bezier(PERFORMANCE_RATING_CURRENT_RUN<<3,0,0,PERFORMANCE_RATING_CURRENT_RUN<<2, graphic_screen_x, graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<2), WHITE);
 }
 void performance_rating_task19() { // draw empty square
-//  draw_empty_square(dword_t x, dword_t y, dword_t width, dword_t height, dword_t color);
+  draw_empty_square(PERFORMANCE_RATING_CURRENT_RUN,PERFORMANCE_RATING_CURRENT_RUN, graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task20() { // draw full square
-//  draw_full_square(dword_t x, dword_t y, dword_t width, dword_t height, dword_t color);
+  draw_full_square(PERFORMANCE_RATING_CURRENT_RUN,PERFORMANCE_RATING_CURRENT_RUN, graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<1), graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task21() { // draw empty circle point
-//  draw_empty_circle_point(dword_t x, dword_t y);
+  draw_empty_circle_point((PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1));
 }
 void performance_rating_task22() { // draw empty circle
-//  draw_empty_circle(dword_t x, dword_t y, dword_t radius, dword_t color);
+  draw_empty_circle((PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1),(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task23() { // draw full circle line
-//  draw_full_circle_line(dword_t x, dword_t y);
+  draw_full_circle_line((PERFORMANCE_RATING_CURRENT_RUN<<1),(PERFORMANCE_RATING_CURRENT_RUN<<2));
 }
 void performance_rating_task24() { // draw full circle
-//  draw_full_circle(dword_t x, dword_t y, dword_t radius, dword_t color);
+  draw_full_circle((PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1),(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task25() { // draw empty ellipse
-//  draw_empty_ellipse(int x0, int y0, int x1, int y1, dword_t color);
+  draw_empty_ellipse((PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1),graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<2),graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task26() { // draw part empty ellipse
-//  draw_parts_of_empty_ellipse(byte_t parts, int x0, int y0, int x1, int y1, dword_t color);
+  draw_parts_of_empty_ellipse(PERFORMANCE_RATING_CURRENT_RUN,(PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1),graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<2),graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
 void performance_rating_task27() { // draw full ellipse
-//  draw_full_ellipse(int x0, int y0, int x1, int y1, dword_t color);
+  draw_full_ellipse((PERFORMANCE_RATING_CURRENT_RUN<<2),(PERFORMANCE_RATING_CURRENT_RUN<<1),graphic_screen_x-(PERFORMANCE_RATING_CURRENT_RUN<<2),graphic_screen_y-(PERFORMANCE_RATING_CURRENT_RUN<<1), WHITE);
 }
-void performance_rating_task28() { //
+void performance_rating_task28() { // full framebuffer
+  redraw_framebuffer();
 }
-void performance_rating_task29() { //
+void performance_rating_task29() { // 25% framebuffer
+  redraw_part_of_framebuffer( graphic_screen_x_center>>1, graphic_screen_y_center>>1, graphic_screen_x>>1, graphic_screen_y>>1);
 }
-void performance_rating_task30() { //
+void performance_rating_task30() { // 
 }
-void performance_rating_task31() { //
+void performance_rating_task31() { // 
 }
+
